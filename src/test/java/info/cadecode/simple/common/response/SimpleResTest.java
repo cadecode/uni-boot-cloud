5f0d81762bc62b1254ed4b1810422741233b7d3c
Cade Li <cade.li@qq.com>
2021-07-16 21:58:04 +0800

    test: 添加工具类 SimpleRes 测试类

---
+++ src/test/java/info/cadecode/simple/common/response/SimpleResTest.java
@@ -0,0 +1,34 @@
+package info.cadecode.simple.common.response;
+
+import info.cadecode.simple.SimpleSpringBootApplicationTests;
+import info.cadecode.simple.constant.Reason;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Cade Li
+ * @date 2021/7/16
+ * @description:
+ */
+public class SimpleResTest extends SimpleSpringBootApplicationTests {
+
+    @Test
+    public void ok() {
+        String okJson = SimpleRes.ok("hello world!")
+                .json();
+        log.info("okJson: {}", okJson);
+    }
+
+    @Test
+    public void error() {
+        String errorJson = SimpleRes.error("hello world!")
+                .json();
+        log.info("errorJson: {}", errorJson);
+    }
+
+    @Test
+    public void reason() {
+        String reasonJson = SimpleRes.reason(Reason.BAD_REQ)
+                .json();
+        log.info("reasonJson: {}", reasonJson);
+    } 
+}
