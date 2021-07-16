8a1cb6ff15cfd1167665f7462c4c78c9c648bb34
Cade Li <cadecode@foxmail.com>
2021-07-16 23:43:45 +0800

    feat: DemoController 添加测试接口

--- src/main/java/info/cadecode/simple/controller/DemoController.java
+++ src/main/java/info/cadecode/simple/controller/DemoController.java
@@ -1,8 +1,12 @@
 package info.cadecode.simple.controller;
 
+import info.cadecode.simple.common.response.SimpleRes;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Cade Li
@@ -17,4 +21,13 @@ public class DemoController {
     public String string() {
         return "DemoController OK!";
     }
+
+    @PostMapping("map")
+    public Map<String, Object> map() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "map");
+        map.put("msg", "hello world");
+        return map;
+    }
+
 }
