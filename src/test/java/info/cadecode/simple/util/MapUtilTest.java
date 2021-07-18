339aaf41d31ac05f406126d41f492e43df638572
Cade Li <cade.li@qq.com>
2021-07-18 18:42:05 +0800

    feat: 添加用于快速创建 map 的 MapUtil 及测试类

---
+++ src/test/java/info/cadecode/simple/util/MapUtilTest.java
@@ -0,0 +1,23 @@
+package info.cadecode.simple.util;
+
+import info.cadecode.simple.SimpleSpringBootApplicationTests;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+/**
+ * @author Cade Li
+ * @date 2021/7/18
+ * @description:
+ */
+public class MapUtilTest extends SimpleSpringBootApplicationTests {
+
+    @Test
+    public void add() {
+        String json = MapUtil.create(new HashMap<String, Integer>())
+                .add("a", 1)
+                .add("b", 2)
+                .asJson();
+        log.info("json: {}", json);
+    }
+}
