6ed36f47c08737c017406f8f1f1f8e11e87912a5
Cade Li <cadecode@foxmail.com>
2021-08-25 18:39:38 +0800

    feat: 添加接口返回 null 时的返回格式处理，抛出异常

--- src/main/java/top/cadecode/simple/common/response/SimpleResAdvice.java
+++ src/main/java/top/cadecode/simple/common/response/SimpleResAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import top.cadecode.simple.common.exception.SimpleException;
+import top.cadecode.simple.constant.ErrorEnum;
 import top.cadecode.simple.util.JsonUtil;
 
 /**
@@ -23,17 +25,21 @@ public class SimpleResAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
                                   Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-
         // 获取请求路径
         String path = request.getURI().getPath();
-        // 根据 body 类型分别处理
+        // 判断 body 是否是 null
+        if (body == null) {
+            throw new SimpleException(ErrorEnum.RES_BODY_INVALID, "接口返回结果为空，path 为 " + path);
+        }
+        // 判断 body 是否是包装好的 SimpleRes
         if (body instanceof SimpleRes) {
             return ((SimpleRes) body).path(path);
         }
-        // String 类型的 Body 需要返回 String 类型，否则报转换错误
+        // 判断 body 是否是包装好的字符串
         if (body instanceof String) {
             // 设置统一的 Content-Type
             response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            // String 类型的 Body 需要返回 String 类型，否则报转换错误
             return JsonUtil.objToStr(SimpleRes.ok(body).path(path));
         }
         return SimpleRes.ok(body).path(path);
