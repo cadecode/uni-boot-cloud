d85062a5f0bff783c51dab2d27e0ade42d1a9aa7
Cade Li <cade.li@qq.com>
2021-08-25 18:38:41 +0800

    feat: 添加 SimpleException 构造函数，传入自定义异常信息

--- src/main/java/top/cadecode/simple/common/exception/SimpleException.java
+++ src/main/java/top/cadecode/simple/common/exception/SimpleException.java
@@ -1,9 +1,7 @@
 package top.cadecode.simple.common.exception;
 
-import lombok.Data;
 import lombok.Getter;
 import top.cadecode.simple.constant.ErrorEnum;
-import top.cadecode.simple.constant.ReasonEnum;
 
 /**
  * @author Cade Li
