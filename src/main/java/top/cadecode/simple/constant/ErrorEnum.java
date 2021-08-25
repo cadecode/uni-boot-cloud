b32bd22d8a88227b59e238ab05c3e4e334df5e93
Cade Li <cade.li@qq.com>
2021-08-25 17:00:53 +0800

    feat: 添加 ErrorEnum 未知错误

--- src/main/java/top/cadecode/simple/constant/ErrorEnum.java
+++ src/main/java/top/cadecode/simple/constant/ErrorEnum.java
@@ -10,11 +10,16 @@ import lombok.Getter;
 @Getter
 public enum ErrorEnum {
 
-    // 认证与权限
+    // 未知错误通用码
+    UNKNOWN(0, "错误原因未知", StatusEnum.FAIL),
+
+    // 认证与权限异常
     NO_TOKEN(1000, "用户未登录", StatusEnum.NO_AUTH),
     TOKEN_ERROR(1001, "用户令牌错误", StatusEnum.NO_AUTH),
     TOKEN_EXPIRED(1002, "用户令牌已过期", StatusEnum.NO_AUTH),
     NO_AUTHORITY(1003, "用户权限不足", StatusEnum.FORBIDDEN),
+    // 系统异常
+
     ;
 
     private final Integer code;
