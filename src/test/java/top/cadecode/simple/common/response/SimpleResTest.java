022ca8d6b3cc29e1cc764cc75dc19b3e636e12c7
Cade Li <cadecode@foxmail.com>
2021-08-25 17:01:46 +0800

    refactor: 重构 SimpleRes

--- src/test/java/top/cadecode/simple/common/response/SimpleResTest.java
+++ src/test/java/top/cadecode/simple/common/response/SimpleResTest.java
@@ -1,8 +1,9 @@
 package top.cadecode.simple.common.response;
 
-import top.cadecode.simple.SimpleSpringBootApplicationTests;
-import top.cadecode.simple.constant.ReasonEnum;
 import org.junit.jupiter.api.Test;
+import top.cadecode.simple.SimpleSpringBootApplicationTests;
+import top.cadecode.simple.constant.ErrorEnum;
+import top.cadecode.simple.util.JsonUtil;
 
 /**
  * @author Cade Li
@@ -13,22 +14,15 @@ public class SimpleResTest extends SimpleSpringBootApplicationTests {
 
     @Test
     public void ok() {
-        String okJson = SimpleRes.ok("hello world!")
-                .json();
+        String okJson = JsonUtil.objToStr(SimpleRes.ok("hello world!"),
+                true);
         log.info("okJson: {}", okJson);
     }
 
     @Test
     public void fail() {
-        String failJson = SimpleRes.fail("hello world!")
-                .json();
+        String failJson = JsonUtil.objToStr(SimpleRes.fail(ErrorEnum.UNKNOWN),
+                true);
         log.info("failJson: {}", failJson);
     }
-
-    @Test
-    public void reason() {
-        String reasonJson = SimpleRes.reason(ReasonEnum.BAD_REQ)
-                .json();
-        log.info("reasonJson: {}", reasonJson);
-    } 
 }
