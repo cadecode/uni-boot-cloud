113887c87b6d0cf9cd72b9408be4e3b5ee6b5543
Cade Li <cade.li@qq.com>
2021-08-25 17:01:46 +0800

    refactor: 重构 SimpleRes

--- src/main/java/top/cadecode/simple/common/response/SimpleRes.java
+++ src/main/java/top/cadecode/simple/common/response/SimpleRes.java
@@ -1,121 +1,113 @@
 package top.cadecode.simple.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-import top.cadecode.simple.common.exception.SimpleException;
-import top.cadecode.simple.constant.ReasonEnum;
-import top.cadecode.simple.util.JsonUtil;
-import top.cadecode.simple.util.MapUtil;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import top.cadecode.simple.constant.ErrorEnum;
+import top.cadecode.simple.constant.StatusEnum;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Cade Li
  * @date 2021/7/16
  * @description: 封装返回响应格式的工具类
  */
-@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SimpleRes {
 
-    private Integer code;
-    private String msg;
+    private Integer status;
+    private String message;
     private String path;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(Include.NON_NULL)
     private Object data;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String error;
-
-    private SimpleRes(ReasonEnum reasonEnum) {
-        this.code = reasonEnum.getCode();
-        this.msg = reasonEnum.getMsg();
-    }
-
-    private SimpleRes(SimpleException exception) {
-        this.code = exception.getCode();
-        this.msg = exception.getMsg();
-    }
+    @JsonInclude(Include.NON_NULL)
+    private ResError error;
 
     /**
-     * 根据 Reason 绑定 code、msg
-     *
-     * @param reasonEnum Reason
-     * @return ResBuilder
+     * 异常信息内部类
      */
-    public static SimpleRes reason(ReasonEnum reasonEnum) {
-        return new SimpleRes(reasonEnum);
+    @Data
+    @AllArgsConstructor
+    public static class ResError {
+        private Integer code;
+        private String reason;
     }
 
     /**
-     * 根据 SimpleException 绑定 code、msg
+     * 返回执行成功的响应
      *
-     * @param exception Reason
-     * @return ResBuilder
-     */
-    public static SimpleRes exception(SimpleException exception) {
-        return new SimpleRes(exception);
-    }
-
-    /**
-     * 返回执行成功的响应 200
-     *
-     * @param data 返回的数据
-     * @return ResBuilder
+     * @param data
+     * @return SimpleRes
      */
     public static SimpleRes ok(Object data) {
-        return reason(ReasonEnum.OK).data(data);
+        SimpleRes res = new SimpleRes();
+        res.setStatus(StatusEnum.OK.getStatus());
+        res.setMessage(StatusEnum.OK.getMessage());
+        res.setData(data);
+        return res;
     }
 
     /**
-     * 返回执行失败的响应 500
+     * 根据 ErrorEnum 返回执行失败响应
      *
-     * @param data 返回的数据
-     * @return ResBuilder
+     * @param data
+     * @return SimpleRes
      */
-    public static SimpleRes fail(Object data) {
-        return reason(ReasonEnum.FAIL).data(data);
+    public static SimpleRes fail(ErrorEnum errorEnum) {
+        SimpleRes res = new SimpleRes();
+        res.setStatus(errorEnum.getStatusEnum().getStatus());
+        res.setMessage(errorEnum.getStatusEnum().getMessage());
+        res.setError(new ResError(errorEnum.getCode(), errorEnum.getReason()));
+        return res;
     }
 
-    public SimpleRes code(Integer code) {
-        this.code = code;
+    /**
+     * 设置 response 的 http 状态码
+     *
+     * @param data
+     * @return SimpleRes
+     */
+    public SimpleRes status(HttpServletResponse response) {
+        response.setStatus(this.getStatus());
         return this;
     }
 
-    public SimpleRes msg(String msg) {
-        this.msg = msg;
-        return this;
-    }
-
+    /**
+     * 设置 path
+     *
+     * @param path
+     * @return SimpleRes
+     */
     public SimpleRes path(String path) {
-        this.path = path;
+        this.setPath(path);
         return this;
     }
 
+    /**
+     * 根据 request 设置 path
+     *
+     * @param path
+     * @return SimpleRes
+     */
+    public SimpleRes path(HttpServletRequest request) {
+        this.setPath(request.getRequestURI());
+        return this;
+    }
+
+    /**
+     * 设置 data
+     *
+     * @param data
+     * @return SimpleRes
+     */
     public SimpleRes data(Object data) {
-        this.data = data;
+        this.setData(data);
         return this;
-    }
-
-    public SimpleRes error(String error) {
-        this.error = error;
-        return this;
-    }
-
-    public String json() {
-        return JsonUtil.objToStr(this);
-    }
-
-    public Map<String, Object> map() {
-        Map<String, Object> map = MapUtil.create()
-                .add("code", code)
-                .add("msg", msg)
-                .add("path", path)
-                .asMap();
-        if (data != null) {
-            map.put("data", data);
-        }
-        if (error != null) {
-            map.put("error", error);
-        }
-        return map;
     }
 }
