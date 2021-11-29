8876157258218175ace947426f191f6d459972d3
Cade Li <cade.li@qq.com>
2021-11-29 19:49:01 +0800

    refactor: 大量重构、拆分模块

---
+++ simple-common/src/main/java/top/cadecode/common/exception/SimpleException.java
@@ -0,0 +1,25 @@
+package top.cadecode.common.exception;
+
+import lombok.Getter;
+import top.cadecode.common.constant.ErrorEnum;
+
+/**
+ * @author Cade Li
+ * @date 2021/7/16
+ * @description 自定义异常类总类
+ */
+@Getter
+public class SimpleException extends RuntimeException {
+
+    private final ErrorEnum errorEnum;
+
+    public SimpleException(ErrorEnum errorEnum) {
+        super(errorEnum.getReason());
+        this.errorEnum = errorEnum;
+    }
+
+    public SimpleException(ErrorEnum errorEnum, String message) {
+        super(message);
+        this.errorEnum = errorEnum;
+    }
+}
---
+++ simple-web/src/test/java/top/cadecode/web/WebApplicationTests.java
@@ -0,0 +1,14 @@
+package top.cadecode.web;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
