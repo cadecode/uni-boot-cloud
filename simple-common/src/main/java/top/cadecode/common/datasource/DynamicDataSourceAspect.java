305136ecf2dbf35e64662e612c5583d4d9ca79c8
Cade Li <cade.li@qq.com>
2021-12-03 23:01:55 +0800

    feat: 优化 DynamicDataSourceAspect，拦截类和方法上的注解

--- simple-common/src/main/java/top/cadecode/common/datasource/DynamicDataSourceAspect.java
+++ simple-common/src/main/java/top/cadecode/common/datasource/DynamicDataSourceAspect.java
@@ -5,6 +5,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import top.cadecode.common.datasource.DynamicDataSource.DynamicDataSourceHolder;
@@ -23,14 +25,27 @@ public class DynamicDataSourceAspect {
     /**
      * 切换数据源
      */
-    @Before("@annotation(dataSource)")
+    @Before("@within(dataSource) || @annotation(dataSource)")
     public void switchDataSource(JoinPoint point, DataSource dataSource) {
-        String dataSourceKey = dataSource.value();
+        // 获取方法上的注解
+        MethodSignature methodSignature = (MethodSignature) point.getSignature();
+        DataSource mDataSource = methodSignature.getMethod().getAnnotation(DataSource.class);
+        // 获取数据源 key
+        String dataSourceKey;
+        if (mDataSource != null) {
+            dataSourceKey = mDataSource.value();
+        } else {
+            // 获取取类上的注解内容
+            dataSourceKey = methodSignature.getMethod()
+                    .getDeclaringClass().
+                    getAnnotation(DataSource.class)
+                    .value();
+        }
+        // 设置数据源
         if (!DynamicDataSourceHolder.containDataSourceKey(dataSourceKey)) {
             log.info("数据源 {} 不存在，将使用默认数据源", dataSource.value());
         } else {
-            // 切换数据源
-            DynamicDataSourceHolder.setDataSourceKey(dataSource.value());
+            DynamicDataSourceHolder.setDataSourceKey(dataSourceKey);
             log.info("切换数据源到 {}，在方法 [{}]", dataSourceKey, point.getSignature());
         }
     }
@@ -38,11 +53,10 @@ public class DynamicDataSourceAspect {
     /**
      * 重置数据源
      */
-    @After("@annotation(dataSource)")
+    @After("@within(dataSource) || @annotation(dataSource)")
     public void resetDataSource(JoinPoint point, DataSource dataSource) {
         // 将数据源重置置为默认数据源
         DynamicDataSourceHolder.clearDataSourceKey();
         log.info("重置默认数据源，在方法 [{}]", point.getSignature());
     }
-
 }
