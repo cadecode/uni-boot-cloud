497d6ffd799b35ca41f51bd8be4f4efca86525b2
Cade Li <cade.li@qq.com>
2021-12-03 20:09:11 +0800

    refactor: 修改数据源配置文件模板，优化数据源配置类

--- simple-common/src/main/java/top/cadecode/common/config/DataSourceConfig.java
+++ simple-common/src/main/java/top/cadecode/common/config/DataSourceConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import top.cadecode.common.datasource.DynamicDataSource;
 import top.cadecode.common.datasource.DynamicDataSource.DynamicDataSourceHolder;
 
@@ -48,16 +50,12 @@ public class DataSourceConfig {
             Map<String, Object> configMap = this.checkDataSourceMap(dataSourceMap, key);
             // 取出数据源类型和 isDefault
             String type = (String) configMap.get("type");
-            Boolean isDefault = (Boolean) configMap.get("isDefault");
-            // 生成数据源实例
-            DataSource dataSource = (DataSource) Binder.get(environment)
-                    .bind(key + ".config", this.checkDataSourceType(type, key)).get();
+            // 配置数据源
+            DataSource dataSource = this.setTargetDataSourceMap(key, type);
             targetDataSourceMap.put(key, dataSource);
             // 判断是否是默认数据源
-            if (isDefault != null && isDefault.equals(true) && !dynamicDataSource.hasDefaultDataSource()) {
-                dynamicDataSource.setDefaultTargetDataSource(dataSource);
-                log.info("设置默认数据源为 " + key);
-            }
+            Boolean isDefault = (Boolean) configMap.get("isDefault");
+            this.setDefaultDataSource(dynamicDataSource, isDefault, dataSource, key);
         });
         // 判断是否设置默认数据源
         if (!dynamicDataSource.hasDefaultDataSource()) {
@@ -67,6 +65,12 @@ public class DataSourceConfig {
         dynamicDataSource.setTargetDataSources(targetDataSourceMap);
         return dynamicDataSource;
     }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dynamicDataSource());
+    }
+
 
     /**
      * 校验 dataSourceMap
@@ -79,19 +83,38 @@ public class DataSourceConfig {
     }
 
     /**
-     * 获取数据源 class
+     * 设置数据源
      */
-    private Class<?> checkDataSourceType(String type, String key) {
-        Class<?> dataSourceClass;
+    private DataSource setTargetDataSourceMap(String key, String type) {
+        Class<DataSource> dataSourceClass;
         try {
-            dataSourceClass = Class.forName(type);
+            dataSourceClass = (Class<DataSource>) Class.forName(type);
             // 判断是否是 DataSource 类型
             if (!DataSource.class.isAssignableFrom(dataSourceClass)) {
                 throw new IllegalArgumentException("数据源 " + key + " 的类型 " + type + " 不合适");
             }
+            // 生成数据源实例
+            DataSource dataSource = (DataSource) Binder.get(environment)
+                    .bind(key + ".config", dataSourceClass).get();
+            return dataSource;
         } catch (ClassNotFoundException e) {
             throw new IllegalArgumentException("数据源 " + key + " 的类型 " + type + " 不存在");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("创建数据源 " + key + " 时出现绑定错误");
         }
-        return dataSourceClass;
+    }
+
+    /**
+     * 配置默认数据源
+     */
+    private void setDefaultDataSource(DynamicDataSource dynamicDataSource, Boolean isDefault,
+                                      DataSource dataSource, String key) {
+        if (dynamicDataSource.hasDefaultDataSource()) {
+            return;
+        }
+        if (isDefault != null && isDefault.equals(true)) {
+            dynamicDataSource.setDefaultTargetDataSource(dataSource);
+            log.info("设置默认数据源为 " + key);
+        }
     }
 }
