dabbd1962ea0c10e9f84d9eb22250ec4e54a7575
Cade Li <cadecode@foxmail.com>
2021-12-05 21:12:48 +0800

    perf: 优化 StringUtil，添加 contains、subString 方法

--- simple-common/src/main/java/top/cadecode/common/util/StringUtil.java
+++ simple-common/src/main/java/top/cadecode/common/util/StringUtil.java
@@ -1,5 +1,7 @@
 package top.cadecode.common.util;
 
+import lombok.NonNull;
+
 /**
  * @author Cade Li
  * @date 2021/7/15
@@ -24,4 +26,49 @@ public class StringUtil {
     public static boolean isTrimEmpty(String str) {
         return str == null || str.trim().length() == 0;
     }
+
+    /**
+     * 判断字符串是否包含指定子串
+     *
+     * @return boolean
+     */
+    public static boolean contains(String str, String subStr) {
+        if (str == null || subStr == null) {
+            return false;
+        }
+        return str.contains(subStr);
+    }
+
+    /**
+     * 截取字符串，支持反序负数
+     *
+     * @param str   字符串
+     * @param begin 开始位置
+     * @return String
+     */
+    public static String subString(String str, int begin) {
+        return subString(str, begin, str.length());
+    }
+
+    /**
+     * 截取字符串，支持反序负数
+     *
+     * @param str   字符串
+     * @param begin 开始位置
+     * @param end   开始位置
+     * @return String
+     */
+    public static String subString(String str, int begin, int end) {
+        if (str == null) {
+            return null;
+        }
+        if (begin < 0) {
+            begin = str.length() + begin;
+        }
+        if (end < 0) {
+            end = str.length() + end;
+        }
+        return str.substring(begin, end);
+    }
+
 }
