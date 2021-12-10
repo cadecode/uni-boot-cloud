5d8a79ceb6b033dd9f2c06a680193475e301568d
Cade Li <cadecode@foxmail.com>
2021-12-10 21:18:19 +0800

    feat: 添加 jwt token 工具类及其配置

---
+++ simple-common/src/main/java/top/cadecode/common/util/TokenUtil.java
@@ -0,0 +1,77 @@
+package top.cadecode.common.util;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.Data;
+import lombok.SneakyThrows;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import top.cadecode.common.core.exception.CommonException;
+import top.cadecode.common.enums.FrameErrorEnum;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Cade Li
+ * @date 2021/12/10
+ * @description Token 工具类
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "simple.jwt")
+public class TokenUtil {
+
+    private String header;
+    private String refreshHeader;
+    private Long expiration;
+    private Long refreshExpiration;
+    private String secret;
+
+    /**
+     * 生成 token
+     *
+     * @param name  用户名
+     * @param roles 角色
+     * @return token 字符串
+     */
+    public String generateToken(String name, List<String> roles) {
+        long expiredTime = new Date().getTime() + expiration * 1000;
+        JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS256);
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .claim("name", name).
+                claim("roles", roles)
+                .expirationTime(new Date(expiredTime)).build();
+        SignedJWT signedJWT = new SignedJWT(jwsHeader, jwtClaimsSet);
+        try {
+            signedJWT.sign(new MACSigner(secret));
+            return signedJWT.serialize();
+        } catch (JOSEException e) {
+            throw CommonException.of(FrameErrorEnum.JWT_CREATE_ERROR).suppressed(e);
+        }
+    }
+
+    /**
+     * 校验 token
+     *
+     * @param token token 字符串
+     * @return 是否通过校验
+     */
+    public Map<String, Object> verifyToken(String token) {
+        try {
+            JWSObject jwsObject = JWSObject.parse(token);
+            JWSVerifier verifier = new MACVerifier(secret);
+            boolean verify = jwsObject.verify(verifier);
+            if (verify) {
+                return jwsObject.getPayload().toJSONObject();
+            }
+            return null;
+        } catch (Exception e) {
+            throw CommonException.of(FrameErrorEnum.JWT_VERIFY_ERROR).suppressed(e);
+        }
+    }
+}
