9fe949762ee21baaf8268c22562d2713e43976ef
Cade Li <cadecode@foxmail.com>
2021-12-11 19:05:28 +0800

    refactor: 配置登录逻辑处理器

--- simple-framework/src/main/java/top/cadecode/framework/config/SecurityConfig.java
+++ simple-framework/src/main/java/top/cadecode/framework/config/SecurityConfig.java
@@ -1,10 +1,20 @@
 package top.cadecode.framework.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import top.cadecode.framework.security.*;
 
 /**
  * @author Cade Li
@@ -14,11 +24,52 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String LOGIN_URL = "/login";
+    public static final String LOGOUT_URL = "/logout";
+
+    private final UserDetailsService userDetailsService;
+    private final LoginSuccessHandler loginSuccessHandler;
+    private final LoginFailureHandler loginFailureHandler;
+    private final SignOutSuccessHandler signOutSuccessHandler;
+    private final NoAuthenticationHandler noAuthenticationHandler;
+    private final NoAuthorityHandler noAuthorityHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭 csrf 和 session
+        http.csrf().disable();
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().permitAll()
+                .loginProcessingUrl(LOGIN_URL)
+                .successHandler(loginSuccessHandler)
+                .failureHandler(loginFailureHandler)
+                .and()
+                .logout().permitAll()
+                .logoutUrl(LOGOUT_URL)
+                .logoutSuccessHandler(signOutSuccessHandler);
+    }
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring()
-                .anyRequest();
+        // web.ignoring()
+        //         .anyRequest();
     }
 }
