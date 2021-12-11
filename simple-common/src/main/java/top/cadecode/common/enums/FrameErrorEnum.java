0a07c9f6b1c3100dce3610f281c0dab870831423
Cade Li <cade.li@qq.com>
2021-12-11 18:20:52 +0800

    feat: 添加 Web 工具类和写入响应 JSON 方法

--- simple-common/src/main/java/top/cadecode/common/enums/FrameErrorEnum.java
+++ simple-common/src/main/java/top/cadecode/common/enums/FrameErrorEnum.java
@@ -14,7 +14,8 @@ public enum FrameErrorEnum implements ResponseCode {
     JSON_TO_STR_ERROR(1000, "对象转 json 字符串出错"),
     JSON_TO_OBJ_ERROR(1001, "json 字符串转对象出错"),
     JWT_CREATE_ERROR(1003, "创建 token 出错"),
-    JWT_VERIFY_ERROR(1003, "解析 token 出错"),
+    JWT_VERIFY_ERROR(1004, "解析 token 出错"),
+    RES_WRITE_JSON_ERROR(1005, "向响应对象写入 JSON 出错 "),
     ;
 
     private final String code;
