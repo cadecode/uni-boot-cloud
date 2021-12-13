972109589ef0f10a507db70bc2cb6e59fc7d211e
Cade Li <cade.li@qq.com>
2021-12-13 23:44:26 +0800

    feat: 添加 JWT 过滤器 refresh token 解析

--- simple-framework/src/main/java/top/cadecode/framework/security/filter/JwtTokenAuthenticationFilter.java
+++ simple-framework/src/main/java/top/cadecode/framework/security/filter/JwtTokenAuthenticationFilter.java
@@ -3,12 +3,14 @@ package top.cadecode.framework.security.filter;
 import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import top.cadecode.common.core.exception.CommonException;
 import top.cadecode.common.core.response.CommonResponse;
 import top.cadecode.common.enums.AuthErrorEnum;
 import top.cadecode.common.util.JsonUtil;
@@ -16,14 +18,15 @@ import top.cadecode.common.util.StringUtil;
 import top.cadecode.common.util.TokenUtil;
 import top.cadecode.common.util.WebUtil;
 import top.cadecode.framework.model.entity.SecurityUser;
+import top.cadecode.framework.model.mapper.SecurityUserMapper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
@@ -36,49 +39,79 @@ import java.util.stream.Collectors;
 public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenUtil tokenUtil;
+    private final SecurityUserMapper securityUserMapper;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        // 获取请求地址
         String requestURI = request.getRequestURI();
         // 取出 token
         String token = request.getHeader(tokenUtil.getHeader());
-        // token 为空，则不拦截
+        // 判断 token 是否为空，为空则不拦截
         if (StringUtil.isEmpty(token)) {
             filterChain.doFilter(request, response);
             return;
         }
+        // 声明一个 SecurityUser，用于设置权限
+        SecurityUser securityUser;
         // 校验 Token
         try {
-            checkToken(request, response, token);
+            // 取出 token 内容
+            JWTClaimsSet claimsSet = tokenUtil.verifyToken(token);
+            long id = tokenUtil.getIdFromClaims(claimsSet);
+            String name = tokenUtil.getNameFromClaims(claimsSet);
+            List<String> roles = tokenUtil.getRolesFromClaims(claimsSet);
+            // 判断 token 过期时间
+            if (!tokenUtil.isExpired(claimsSet.getExpirationTime())) {
+                // 如果 token 没有过期
+                // 根据 token 内容绑定 securityUser
+                securityUser = new SecurityUser();
+                securityUser.setId(id);
+                securityUser.setUsername(name);
+                securityUser.setAuthorities(roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList()));
+                setAuthentication(request, securityUser);
+                filterChain.doFilter(request, response);
+                return;
+            }
+            // 如果 token 已过期
+            String refreshHeader = request.getHeader(tokenUtil.getRefreshHeader());
+            // 判断 refreshToken 是否为空
+            if (StringUtil.isEmpty(refreshHeader)) {
+                writeResponse(response, AuthErrorEnum.TOKEN_EXPIRED, requestURI);
+                return;
+            }
+            // 根据 username 查询用户
+            securityUser = (SecurityUser) userDetailsService.loadUserByUsername(name);
+            // 判断 refreshToken 是否相符
+            if (securityUser == null || !refreshHeader.equals(securityUser.getRefreshToken())) {
+                writeResponse(response, AuthErrorEnum.TOKEN_REFRESH_ERROR, requestURI);
+                return;
+            }
+            // 判断 refreshToken 是否过期
+            if (securityUser.getTokenTime() == null
+                    || tokenUtil.isExpired(securityUser.getTokenTime(), tokenUtil.getRefreshExpiration())) {
+                writeResponse(response, AuthErrorEnum.TOKEN_REFRESH_EXPIRED, requestURI);
+                return;
+            }
+            // 重颁发 token 并设置响应头
+            List<String> newRoles = securityUser.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+            String newToken = tokenUtil.generateToken(securityUser.getId(), securityUser.getUsername(), newRoles);
+            String newRefreshToken = UUID.randomUUID().toString();
+            response.addHeader(tokenUtil.getHeader(), newToken);
+            response.addHeader(tokenUtil.getRefreshHeader(), newRefreshToken);
+            // 更新 refresh token
+            securityUserMapper.updateSecurityUserToken(securityUser.getId(), newRefreshToken);
+            setAuthentication(request, securityUser);
             filterChain.doFilter(request, response);
         } catch (Exception e) {
             writeResponse(response, AuthErrorEnum.TOKEN_ERROR, requestURI);
         }
-    }
-
-    private void checkToken(HttpServletRequest request, HttpServletResponse response, String token) throws Exception {
-        // 取出 token 内容
-        JWTClaimsSet claimsSet = tokenUtil.verifyToken(token);
-        // 判断过期时间
-        if (claimsSet.getExpirationTime().getTime() < System.currentTimeMillis()) {
-            writeResponse(response, AuthErrorEnum.TOKEN_EXPIRED, request.getRequestURI());
-            return;
-        }
-        // 认证通过，设置认证信息
-        long id = claimsSet.getLongClaim("id");
-        List<String> roles = claimsSet.getStringListClaim("roles");
-        SecurityUser securityUser = new SecurityUser();
-        securityUser.setId(id);
-        securityUser.setAuthorities(roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList()));
-        // 构造 UsernamePasswordAuthenticationToken
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(securityUser, null, securityUser.getAuthorities());
-        // 设置认证信息，用于后面的过滤器使用
-        authentication.setDetails(new WebAuthenticationDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     private void writeResponse(HttpServletResponse response, AuthErrorEnum authErrorEnum, String requestURI) {
@@ -86,4 +119,12 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
         WebUtil.writeJsonToResponse(response, JsonUtil.objToStr(commonResponse));
     }
 
+    private void setAuthentication(HttpServletRequest request, UserDetails userDetails) {
+        // 构造 UsernamePasswordAuthenticationToken
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        // 设置认证信息，用于后面的过滤器使用
+        authentication.setDetails(new WebAuthenticationDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 }
