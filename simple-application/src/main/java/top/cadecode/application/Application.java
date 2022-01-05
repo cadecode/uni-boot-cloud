5d31cc72c5451d8af2299e78c518524f9ec679b6
Cade Li <cade.li@qq.com>
2022-01-05 15:55:10 +0800

    fix: 修改 mapper 扫描路径，避免 mybatis 代理无辜接口

--- simple-application/src/main/java/top/cadecode/application/Application.java
+++ simple-application/src/main/java/top/cadecode/application/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("top.cadecode")
+@MapperScan("top.cadecode.**.mapper")
 @ComponentScan("top.cadecode")
 public class Application {
 
