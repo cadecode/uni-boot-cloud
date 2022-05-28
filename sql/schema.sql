538ba33892c0671936ebe3b1a6e4fbc536b64824
Cade Li <cadecode@foxmail.com>
2022-05-28 23:25:21 +0800

    chore: 更新表结构 SQL

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,16 @@
 -- ----------------------------
 -- 系统用户表
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS security_user
+CREATE TABLE IF NOT EXISTS sys_user
 (
-    id            BIGINT UNSIGNED AUTO_INCREMENT,
-    username      VARCHAR(50)      NOT NULL COMMENT '用户名',
-    password      VARCHAR(100)     NOT NULL COMMENT '密码',
-    nick_name     VARCHAR(50)      NOT NULL COMMENT '昵称',
-    enable_flag   TINYINT UNSIGNED NOT NULL COMMENT '是否启用',
-    create_time   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_time   TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    id          BIGINT UNSIGNED,
+    username    VARCHAR(50)  NOT NULL COMMENT '用户名',
+    password    VARCHAR(100) NOT NULL COMMENT '密码',
+    nick_name   VARCHAR(50)  NOT NULL COMMENT '昵称',
+    enable_flag TINYINT      NOT NULL COMMENT '是否启用',
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    update_user VARCHAR(100) NULL,
     PRIMARY KEY (id),
     UNIQUE KEY uk_username (username)
 ) ENGINE = InnoDB
@@ -18,14 +19,15 @@ CREATE TABLE IF NOT EXISTS security_user
 -- ----------------------------
 -- 系统角色表
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS security_role
+CREATE TABLE IF NOT EXISTS sys_role
 (
-    id          BIGINT UNSIGNED AUTO_INCREMENT,
-    code        VARCHAR(50) NOT NULL COMMENT '角色代码',
-    name        VARCHAR(50) NOT NULL COMMENT '角色名称',
+    id          BIGINT UNSIGNED,
+    code        VARCHAR(50)  NOT NULL COMMENT '角色代码',
+    name        VARCHAR(50)  NOT NULL COMMENT '角色名称',
     description VARCHAR(100) COMMENT '角色描述',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    update_user VARCHAR(100) NULL,
     PRIMARY KEY (id),
     UNIQUE KEY uk_code (code)
 ) ENGINE = InnoDB
@@ -34,13 +36,14 @@ CREATE TABLE IF NOT EXISTS security_role
 -- ----------------------------
 -- 系统权限表
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS security_api
+CREATE TABLE IF NOT EXISTS sys_api
 (
-    id          BIGINT UNSIGNED AUTO_INCREMENT,
-    url         VARCHAR(50) NOT NULL COMMENT '接口路径',
+    id          BIGINT UNSIGNED,
+    url         VARCHAR(50)  NOT NULL COMMENT '接口路径',
     description VARCHAR(100) COMMENT '接口描述',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    update_user VARCHAR(100) NULL,
     PRIMARY KEY (id),
     UNIQUE KEY uk_url (url)
 ) ENGINE = InnoDB
@@ -49,7 +52,7 @@ CREATE TABLE IF NOT EXISTS security_api
 -- ----------------------------
 -- 角色用户关系表
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS security_role_user
+CREATE TABLE IF NOT EXISTS sys_role_user
 (
     role_id BIGINT UNSIGNED NOT NULL,
     user_id BIGINT UNSIGNED NOT NULL,
@@ -61,7 +64,7 @@ CREATE TABLE IF NOT EXISTS security_role_user
 -- ----------------------------
 -- 角色权限关系表
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS security_role_api
+CREATE TABLE IF NOT EXISTS sys_role_api
 (
     role_id BIGINT UNSIGNED NOT NULL,
     api_id  BIGINT UNSIGNED NOT NULL,
