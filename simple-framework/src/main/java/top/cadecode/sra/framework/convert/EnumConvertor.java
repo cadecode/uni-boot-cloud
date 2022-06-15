203e55d0698bb9bfc7579df2cd4c70b8c7701fe2
Cade Li <cade.li@qq.com>
2022-06-15 20:38:52 +0800

    feat: 添加接口接收枚举类的通用转换器

---
+++ simple-framework/src/main/java/top/cadecode/sra/framework/convert/EnumConvertor.java
@@ -0,0 +1,15 @@
+package top.cadecode.sra.framework.convert;
+
+/**
+ * @author Cade Li
+ * @date 2022/5/28
+ * @description 请求参数枚举转换接口
+ */
+public interface EnumConvertor {
+
+    /**
+     * 返回枚举元素的对应标记
+     */
+    String convertBy();
+
+}
