40b627ceb63cc9b0863a15fc9cb78f664b1822d7
Cade Li <cade.li@qq.com>
2022-06-16 21:35:41 +0800

    feat: 优化 Mybatis 枚举处理器，默认使用 ordinal 处理

--- simple-framework/src/main/java/top/cadecode/sra/framework/convert/mybatis/DefaultEnumTypeHandler.java
+++ simple-framework/src/main/java/top/cadecode/sra/framework/convert/mybatis/DefaultEnumTypeHandler.java
@@ -17,15 +17,16 @@ import static java.util.stream.Collectors.toMap;
 /**
  * @author Cade Li
  * @date 2022/6/16
- * @description MyBatis 枚举类解析器
+ * @description MyBatis 枚举类解析器，需要配置成默认枚举解析器使用，
+ * 枚举类实现 EnumConvertor 接口可自定义持久化属性，没有实现 EnumConvertor 的普通枚举将使用 ordinal 进行持久化
  */
-public class MyBatisEnumTypeHandler<E extends EnumConvertor> extends BaseTypeHandler<E> {
+public class DefaultEnumTypeHandler<E extends Enum<?>> extends BaseTypeHandler<E> {
 
     private final Class<E> type;
 
     private final Map<Integer, E> enumMap;
 
-    public MyBatisEnumTypeHandler(Class<E> type) {
+    public DefaultEnumTypeHandler(Class<E> type) {
         if (Objects.isNull(type)) {
             throw new IllegalArgumentException("类型不能为空");
         }
@@ -34,17 +35,23 @@ public class MyBatisEnumTypeHandler<E extends EnumConvertor> extends BaseTypeHan
         if (Objects.isNull(enums)) {
             throw new IllegalArgumentException(type.getSimpleName() + " 不是一个枚举类型");
         }
-        enumMap = Arrays.stream(enums).collect(toMap(EnumConvertor::persistBy, o -> o, (p, n) -> p));
+        enumMap = Arrays.stream(enums).collect(toMap(o -> {
+            // 实现 EnumConvertor 的枚举根据 persistBy 持久化
+            if (o instanceof EnumConvertor) {
+                return ((EnumConvertor) o).persistBy();
+            }
+            return o.ordinal();
+        }, o -> o, (p, n) -> p));
     }
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, E parameter, JdbcType jdbcType) throws SQLException {
         // 默认将标记按 int 入库
         if (Objects.isNull(jdbcType)) {
-            ps.setInt(i, parameter.persistBy());
+            ps.setInt(i, valueFromEnum(parameter));
             return;
         }
-        ps.setObject(i, parameter.persistBy(), jdbcType.TYPE_CODE);
+        ps.setObject(i, valueFromEnum(parameter), jdbcType.TYPE_CODE);
     }
 
     @Override
@@ -53,7 +60,7 @@ public class MyBatisEnumTypeHandler<E extends EnumConvertor> extends BaseTypeHan
         if (rs.wasNull()) {
             return null;
         } else {
-            return enumFromFlag(i);
+            return enumFromValue(i);
         }
     }
 
@@ -63,7 +70,7 @@ public class MyBatisEnumTypeHandler<E extends EnumConvertor> extends BaseTypeHan
         if (rs.wasNull()) {
             return null;
         } else {
-            return enumFromFlag(i);
+            return enumFromValue(i);
         }
     }
 
@@ -73,18 +80,28 @@ public class MyBatisEnumTypeHandler<E extends EnumConvertor> extends BaseTypeHan
         if (cs.wasNull()) {
             return null;
         } else {
-            return enumFromFlag(i);
+            return enumFromValue(i);
         }
     }
 
     /**
      * 从 enum 元素中提取指定元素
      */
-    private E enumFromFlag(Integer value) {
+    private E enumFromValue(Integer value) {
         E e = enumMap.get(value);
         if (Objects.nonNull(e)) {
             return e;
         }
         throw new IllegalArgumentException("未知的泛型元素：" + type.getSimpleName() + "." + value);
+    }
+
+    /**
+     * 从 enum 元素中获取 value
+     */
+    private Integer valueFromEnum(E e) {
+        if (e instanceof EnumConvertor) {
+            return ((EnumConvertor) e).persistBy();
+        }
+        return e.ordinal();
     }
 }
