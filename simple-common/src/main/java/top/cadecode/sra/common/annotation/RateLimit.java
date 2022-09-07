6dc07d197f0b0336cce4933270204807db552133
Cade Li <cade.li@qq.com>
2022-09-07 09:16:14 +0800

    feat: 添加单机限流器注解和 AOP 切面

---
+++ simple-common/src/main/java/top/cadecode/sra/common/annotation/RateLimit.java
@@ -0,0 +1,42 @@
+package top.cadecode.sra.common.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Cade Li
+ * @date 2022/9/4
+ * @description 限流注解
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RateLimit {
+
+
+    @AliasFor("limitPerSecond")
+    double value() default 1;
+
+    /**
+     * 每秒限制数量
+     */
+    @AliasFor("value")
+    double limitPerSecond() default 1;
+
+    /**
+     * 是否阻塞等待
+     */
+    boolean blockWait() default false;
+
+    /**
+     * 等待时间
+     */
+    long time() default 0;
+
+    /**
+     * 等待时间单位
+     */
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+}
