02718cdfddafe0990f9756e5a23236447489bae0
Cade Li <cadecode@foxmail.com>
2022-09-26 13:43:33 +0800

    feat: AssertUtil 增加工具方法

--- uni-boot-common/src/main/java/top/cadecode/uniboot/common/util/AssertUtil.java
+++ uni-boot-common/src/main/java/top/cadecode/uniboot/common/util/AssertUtil.java
@@ -1,9 +1,8 @@
 package top.cadecode.uniboot.common.util;
 
+import cn.hutool.core.util.ObjectUtil;
 import top.cadecode.uniboot.common.exception.UniErrorCode;
 import top.cadecode.uniboot.common.exception.UniException;
-
-import java.util.Objects;
 
 /**
  * 断言工具类
@@ -59,13 +58,56 @@ public class AssertUtil {
     }
 
     /**
+     * 判断是否为 false，如果是就抛出运行时异常
+     *
+     * @param expression 布尔表达式
+     * @param exception  运行时异常
+     */
+    public static void isFalse(boolean expression, RuntimeException exception) {
+        isTrue(!expression, exception);
+    }
+
+    /**
+     * 判断是否为 false，如果是就抛出运行时异常 UniException
+     *
+     * @param expression 布尔表达式
+     * @param moreInfo   异常信息
+     */
+    public static void isFalse(boolean expression, String moreInfo) {
+        isTrue(!expression, UniException.of(moreInfo));
+    }
+
+    /**
+     * 判断是否为 false，如果是就抛出运行时异常 UniException
+     *
+     * @param expression 布尔表达式
+     * @param errorCode  错误码
+     * @param moreInfo   异常信息
+     */
+    public static void isFalse(boolean expression, UniErrorCode errorCode, String moreInfo) {
+        isTrue(!expression, UniException.of(errorCode, moreInfo));
+    }
+
+    /**
+     * 判断是否为 false，如果是就抛出运行时异常 UniException，指定 caused
+     *
+     * @param expression 布尔表达式
+     * @param errorCode  错误码
+     * @param throwable  caused 异常
+     * @param moreInfo   异常信息
+     */
+    public static void isFalse(boolean expression, UniErrorCode errorCode, Throwable throwable, String moreInfo) {
+        isTrue(!expression, UniException.of(errorCode, throwable, moreInfo));
+    }
+
+    /**
      * 判断是否为 null，如果是就抛出运行时异常
      *
      * @param o         对象
      * @param exception 运行时异常
      */
     public static void isNull(Object o, RuntimeException exception) {
-        isTrue(Objects.isNull(o), exception);
+        isTrue(ObjectUtil.isNull(o), exception);
     }
 
     /**
@@ -75,7 +117,7 @@ public class AssertUtil {
      * @param moreInfo 异常信息
      */
     public static void isNull(Object o, String moreInfo) {
-        isNull(o, UniException.of(moreInfo));
+        isTrue(ObjectUtil.isNull(o), UniException.of(moreInfo));
     }
 
     /**
@@ -86,7 +128,7 @@ public class AssertUtil {
      * @param moreInfo  异常信息
      */
     public static void isNull(Object o, UniErrorCode errorCode, String moreInfo) {
-        isNull(o, UniException.of(errorCode, moreInfo));
+        isTrue(ObjectUtil.isNull(o), UniException.of(errorCode, moreInfo));
     }
 
     /**
@@ -98,6 +140,136 @@ public class AssertUtil {
      * @param moreInfo  异常信息
      */
     public static void isNull(Object o, UniErrorCode errorCode, Throwable throwable, String moreInfo) {
-        isNull(o, UniException.of(errorCode, throwable, moreInfo));
+        isTrue(ObjectUtil.isNull(o), UniException.of(errorCode, throwable, moreInfo));
     }
+
+    /**
+     * 判断是否不为 null，如果是就抛出运行时异常
+     *
+     * @param o         对象
+     * @param exception 运行时异常
+     */
+    public static void isNotNull(Object o, RuntimeException exception) {
+        isTrue(ObjectUtil.isNotNull(o), exception);
+    }
+
+    /**
+     * 判断是否不为 null，如果是就抛出运行时异常 UniException
+     *
+     * @param o        对象
+     * @param moreInfo 异常信息
+     */
+    public static void isNotNull(Object o, String moreInfo) {
+        isTrue(ObjectUtil.isNotNull(o), UniException.of(moreInfo));
+    }
+
+    /**
+     * 判断是否不为 null，如果是就抛出运行时异常 UniException
+     *
+     * @param o         对象
+     * @param errorCode 错误码
+     * @param moreInfo  异常信息
+     */
+    public static void isNotNull(Object o, UniErrorCode errorCode, String moreInfo) {
+        isTrue(ObjectUtil.isNotNull(o), UniException.of(errorCode, moreInfo));
+    }
+
+    /**
+     * 判断是否不为 null，如果是就抛出运行时异常 UniException，指定 caused
+     *
+     * @param o         布尔表达式
+     * @param errorCode 对象
+     * @param throwable caused 异常
+     * @param moreInfo  异常信息
+     */
+    public static void isNotNull(Object o, UniErrorCode errorCode, Throwable throwable, String moreInfo) {
+        isTrue(ObjectUtil.isNotNull(o), UniException.of(errorCode, throwable, moreInfo));
+    }
+
+    /**
+     * 判断是否为空，如果是就抛出运行时异常
+     *
+     * @param o         对象
+     * @param exception 运行时异常
+     */
+    public static void isEmpty(Object o, RuntimeException exception) {
+        isTrue(ObjectUtil.isEmpty(o), exception);
+    }
+
+    /**
+     * 判断是否为空，如果是就抛出运行时异常 UniException
+     *
+     * @param o        对象
+     * @param moreInfo 异常信息
+     */
+    public static void isEmpty(Object o, String moreInfo) {
+        isTrue(ObjectUtil.isEmpty(o), UniException.of(moreInfo));
+    }
+
+    /**
+     * 判断是否为空，如果是就抛出运行时异常 UniException
+     *
+     * @param o         对象
+     * @param errorCode 错误码
+     * @param moreInfo  异常信息
+     */
+    public static void isEmpty(Object o, UniErrorCode errorCode, String moreInfo) {
+        isTrue(ObjectUtil.isEmpty(o), UniException.of(errorCode, moreInfo));
+    }
+
+    /**
+     * 判断是否为空，如果是就抛出运行时异常 UniException，指定 caused
+     *
+     * @param o         布尔表达式
+     * @param errorCode 对象
+     * @param throwable caused 异常
+     * @param moreInfo  异常信息
+     */
+    public static void isEmpty(Object o, UniErrorCode errorCode, Throwable throwable, String moreInfo) {
+        isTrue(ObjectUtil.isEmpty(o), UniException.of(errorCode, throwable, moreInfo));
+    }
+
+    /**
+     * 判断是否不为空，如果是就抛出运行时异常
+     *
+     * @param o         对象
+     * @param exception 运行时异常
+     */
+    public static void isNotEmpty(Object o, RuntimeException exception) {
+        isTrue(ObjectUtil.isNotEmpty(o), exception);
+    }
+
+    /**
+     * 判断是否不为空，如果是就抛出运行时异常 UniException
+     *
+     * @param o        对象
+     * @param moreInfo 异常信息
+     */
+    public static void isNotEmpty(Object o, String moreInfo) {
+        isTrue(ObjectUtil.isNotEmpty(o), UniException.of(moreInfo));
+    }
+
+    /**
+     * 判断是否不为空，如果是就抛出运行时异常 UniException
+     *
+     * @param o         对象
+     * @param errorCode 错误码
+     * @param moreInfo  异常信息
+     */
+    public static void isNotEmpty(Object o, UniErrorCode errorCode, String moreInfo) {
+        isTrue(ObjectUtil.isNotEmpty(o), UniException.of(errorCode, moreInfo));
+    }
+
+    /**
+     * 判断是否不为空，如果是就抛出运行时异常 UniException，指定 caused
+     *
+     * @param o         布尔表达式
+     * @param errorCode 对象
+     * @param throwable caused 异常
+     * @param moreInfo  异常信息
+     */
+    public static void isNotEmpty(Object o, UniErrorCode errorCode, Throwable throwable, String moreInfo) {
+        isTrue(ObjectUtil.isNotEmpty(o), UniException.of(errorCode, throwable, moreInfo));
+    }
+
 }
