2b04bb02208e1717680006abb15a69801fee106b
Cade Li <cadecode@foxmail.com>
2022-10-08 15:22:04 +0800

    feat: 引入 Spring Retry，添加测试代码 SpringRetryController

---
+++ uni-boot-framework/src/main/java/top/cadecode/uniboot/framework/config/RetryConfig.java
@@ -0,0 +1,35 @@
+package top.cadecode.uniboot.framework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+/**
+ * Spring Retry 配置类
+ *
+ * @author Cade Li
+ * @date 2022/9/28
+ */
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+    @Bean
+    public RetryTemplate defaultRetryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+        // 延迟规则
+        // 初次重试间隔 1 秒，后面每次乘以 2
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(1000L);
+        backOffPolicy.setMultiplier(2);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+        return retryTemplate;
+    }
+
+}
