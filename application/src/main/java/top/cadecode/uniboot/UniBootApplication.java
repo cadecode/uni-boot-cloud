0ce9f728f74c5223a5e5486a5fce29b429ab78d3
Cade Li <cade.li@qq.com>
2023-02-23 19:48:57 +0800

    feat: 集成 pagehelper

--- application/src/main/java/top/cadecode/uniboot/UniBootApplication.java
+++ application/src/main/java/top/cadecode/uniboot/UniBootApplication.java
@@ -1,17 +1,17 @@
 package top.cadecode.uniboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  *
  */
-@ComponentScan("top.cadecode")
+@MapperScan("top.decode.**.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UniBootApplication extends SpringBootServletInitializer {
