6ddce5e2a1233291e49a200556130035d841f735
Cade Li <cadecode@foxmail.com>
2023-04-12 20:04:54 +0800

    feat: 优化注释、注解

--- system/src/main/java/top/cadecode/uniboot/system/bean/vo/SysApiVo.java
+++ system/src/main/java/top/cadecode/uniboot/system/bean/vo/SysApiVo.java
@@ -1,7 +1,5 @@
 package top.cadecode.uniboot.system.bean.vo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.util.List;
@@ -15,24 +13,11 @@ import java.util.List;
 @Data
 public class SysApiVo {
 
-    /**
-     * id
-     */
-    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
-    /**
-     * 接口 url
-     */
     private String url;
 
-    /**
-     * 描述
-     */
     private String description;
 
-    /**
-     * 角色列表
-     */
     private List<String> roles;
 }
--- system/src/main/java/top/cadecode/uniboot/system/bean/vo/SysUserVo.java
+++ system/src/main/java/top/cadecode/uniboot/system/bean/vo/SysUserVo.java
@@ -1,7 +1,5 @@
 package top.cadecode.uniboot.system.bean.vo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.util.Date;
@@ -16,54 +14,23 @@ import java.util.List;
 @Data
 public class SysUserVo {
 
-    /**
-     * id
-     */
-    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
-    /**
-     * 用户名
-     */
     private String username;
 
-    /**
-     * 昵称
-     */
     private String nickName;
 
-    /**
-     * 是否启用
-     */
     private boolean enableFlag;
 
-    /**
-     * 性别
-     */
     private String sex;
 
-    /**
-     * 邮箱
-     */
     private String mail;
 
-    /**
-     * 电话
-     */
     private String phone;
 
-    /**
-     * 登录 IP
-     */
     private String loginIp;
 
-    /**
-     * 登录时间
-     */
     private Date loginDate;
 
-    /**
-     * 角色
-     */
     private List<String> roles;
 }
