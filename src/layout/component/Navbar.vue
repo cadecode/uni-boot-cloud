6605dcf3913e0f073dcf29d05341f0c1e188611f
Cade Li <cade.li@qq.com>
2023-04-19 19:12:47 +0800

    feat: 修改头像下拉框选项

--- src/layout/component/Navbar.vue
+++ src/layout/component/Navbar.vue
@@ -13,13 +13,13 @@
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
-            <el-dropdown-item> Home</el-dropdown-item>
+            <el-dropdown-item>首页</el-dropdown-item>
           </router-link>
           <a target="_blank" href="https://github.com/cadecode/uni-boot-admin-vue">
             <el-dropdown-item>Github</el-dropdown-item>
           </a>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">注销</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
