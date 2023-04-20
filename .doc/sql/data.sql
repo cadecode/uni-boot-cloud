1ce1dc007feef6927d0db6304f1c4f3a061c8caf
Cade Li <cade.li@qq.com>
2023-04-20 13:31:36 +0800

    docs: 添加menu测试数据

--- .doc/sql/data.sql
+++ .doc/sql/data.sql
@@ -28,23 +28,40 @@ INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu
 
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (2, null, 'SystemManagement', '/system_management', null, '系统管理', 0, 1, 1);
+    VALUE (2, null, 'System', '/system', null, '系统管理', 0, 1, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (3, 2, 'UserManagement', '/system_management/user', '/SystemManagement/User', '用户管理', 1, 101, 1);
+    VALUE (3, 2, 'User', '/system/user', '/System/User', '用户管理', 1, 101, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (4, 2, 'RoleManagement', '/system_management/role', '/SystemManagement/Role', '角色管理', 1, 102, 1);
+    VALUE (4, 2, 'Role', '/system/role', '/System/Role', '角色管理', 1, 102, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (5, 2, 'MenuManagement', '/system_management/menu', '/SystemManagement/Menu', '菜单管理', 1, 103, 1);
+    VALUE (5, 2, 'Menu', '/system/menu', '/System/Menu', '菜单管理', 1, 103, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (6, 2, 'ApiManagement', '/system_management/api', '/SystemManagement/Api', 'API管理', 1, 104, 1);
+    VALUE (6, 2, 'Api', '/system/api', '/System/Api', 'API管理', 1, 104, 1);
 
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (7, null, 'GithubUrl', 'https://github.com/cadecode/uni-boot-admin', null, '源码仓库', 1, 2, 1);
+    VALUE (7, null, 'Develop', '/develop', null, '开发管理', 0, 2, 1);
+INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
+                     enable_flag)
+    VALUE (8, 7, 'Icon', '/develop/icon', '/Develop/Icon', '图标', 1, 201, 1);
+INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
+                     enable_flag)
+    VALUE (9, 7, 'Swagger', 'http://localhost:8080/doc.html', null, 'Swagger', 1, 202, 1);
+INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
+                     enable_flag)
+    VALUE (10, 7, 'Cache', '/develop/cache', '/Develop/Cache', '缓存管理', 1, 203, 1);
+INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
+                     enable_flag)
+    VALUE (11, 7, 'ServerInfo', '/develop/server_info', '/Develop/ServerInfo', '服务器信息', 1,
+           204, 1);
+
+INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
+                     enable_flag)
+    VALUE (100, null, 'GithubUrl', 'https://github.com/cadecode/uni-boot-admin', null, '源码仓库', 1, 100, 1);
 -- ----------------------------
 -- 插入 API
 -- ----------------------------
@@ -77,11 +94,35 @@ VALUES (1, 6);
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (1, 7);
 INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 8);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 9);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 10);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 11);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 100);
+
+INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (2, 1);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 7);
+VALUES (2, 2);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (2, 3);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (2, 4);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (2, 5);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (2, 6);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (2, 100);
+
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (3, 1);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (3, 100);
 -- ----------------------------
 -- 插入角色API关系
 -- ----------------------------
