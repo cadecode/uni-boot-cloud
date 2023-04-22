3b01cf091d304ce5684056365feb04a1dea0730e
Cade Li <cadecode@foxmail.com>
2023-04-22 22:54:05 +0800

    feat: 添加修改用户信息和密码接口

--- application/src/main/java/top/cadecode/uniboot/controller/SystemController.java
+++ application/src/main/java/top/cadecode/uniboot/controller/SystemController.java
@@ -1,19 +1,29 @@
 package top.cadecode.uniboot.controller;
 
+import cn.hutool.core.util.ObjectUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.cadecode.uniboot.common.annotation.ApiFormat;
+import top.cadecode.uniboot.common.exception.UniException;
 import top.cadecode.uniboot.framework.security.TokenAuthHolder;
 import top.cadecode.uniboot.system.bean.dto.SysUserDto.SysUserDetailsDto;
 import top.cadecode.uniboot.system.bean.dto.SysUserDto.SysUserInfoDto;
+import top.cadecode.uniboot.system.bean.po.SysUser;
 import top.cadecode.uniboot.system.bean.vo.SysMenuVo.SysMenuTreeVo;
+import top.cadecode.uniboot.system.request.SysUserRequest.SysUserModifyInfoRequest;
+import top.cadecode.uniboot.system.request.SysUserRequest.SysUserModifyPassRequest;
 import top.cadecode.uniboot.system.service.SysMenuService;
+import top.cadecode.uniboot.system.service.SysUserService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -28,9 +38,13 @@ import java.util.List;
 @Api(tags = "系统管理")
 @RequestMapping("system")
 @RestController
+@Validated
 public class SystemController {
 
+    private final SysUserService sysUserService;
     private final SysMenuService sysMenuService;
+
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 获取用户信息
@@ -42,4 +56,37 @@ public class SystemController {
         List<SysMenuTreeVo> sysMenuTreeVos = sysMenuService.listTreeVoByRoles(userDetails.getRoles());
         return SysUserInfoDto.builder().menuList(sysMenuTreeVos).build();
     }
+
+    @ApiOperation("修改用户信息")
+    @PostMapping("user/modify_info")
+    public boolean userModifyInfo(@RequestBody @Valid SysUserModifyInfoRequest request) {
+        SysUserDetailsDto userDetails = TokenAuthHolder.getUserDetails(null);
+        return sysUserService.lambdaUpdate()
+                .eq(SysUser::getId, userDetails.getId())
+                .set(SysUser::getNickName, request.getNickName())
+                .set(SysUser::getPhone, request.getPhone())
+                .set(SysUser::getMail, request.getMail())
+                .set(SysUser::getSex, request.getSex())
+                .update();
+    }
+
+    @ApiOperation("修改用户密码")
+    @PostMapping("user/modify_pass")
+    public boolean userModifyPass(@RequestBody @Valid SysUserModifyPassRequest request) {
+        SysUserDetailsDto userDetails = TokenAuthHolder.getUserDetails(null);
+        SysUser sysUser = sysUserService.lambdaQuery().select(SysUser::getPassword)
+                .eq(SysUser::getId, userDetails.getId()).one();
+        if (ObjectUtil.notEqual(request.getNewPass(), request.getConfirmedPass())){
+            throw UniException.of("新密码和确认密码不一致");
+        }
+        if (!passwordEncoder.matches(request.getOldPass(), sysUser.getPassword())) {
+            throw UniException.of("原密码错误");
+        }
+        return sysUserService.lambdaUpdate()
+                .eq(SysUser::getId, userDetails.getId())
+                .eq(SysUser::getPassword, sysUser.getPassword())
+                .set(SysUser::getPassword, passwordEncoder.encode(request.getNewPass()))
+                .update();
+    }
+
 }
