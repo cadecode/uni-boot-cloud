542e09a0230d2f8d9df8aa9ac770fbdc6b3ea2df
Cade Li <cade.li@qq.com>
2023-04-24 15:09:50 +0800

    feat: 添加根据菜单、api查询角色的方法

--- system/src/main/java/top/cadecode/uniboot/system/service/SysRoleService.java
+++ system/src/main/java/top/cadecode/uniboot/system/service/SysRoleService.java
@@ -3,6 +3,8 @@ package top.cadecode.uniboot.system.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import top.cadecode.uniboot.system.bean.po.SysRole;
 
+import java.util.List;
+
 /**
  * 系统角色服务
  *
@@ -10,4 +12,10 @@ import top.cadecode.uniboot.system.bean.po.SysRole;
  * @date 2022/5/27
  */
 public interface SysRoleService extends IService<SysRole> {
+
+    List<SysRole> listByUserId( Long userId);
+
+    List<SysRole> listByMenuId(Long menuId);
+
+    List<SysRole> listByApiId(Long apiId);
 }
