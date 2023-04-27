1d603277cc0a60505d84a8a5c097533be85f1cea
Cade Li <cade.li@qq.com>
2023-04-27 10:33:53 +0800

    feat: 添加mybatis plus自动填充注解

--- system/src/main/java/top/cadecode/uniboot/system/bean/po/SysApi.java
+++ system/src/main/java/top/cadecode/uniboot/system/bean/po/SysApi.java
@@ -1,6 +1,8 @@
 package top.cadecode.uniboot.system.bean.po;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
@@ -31,9 +33,12 @@ public class SysApi {
      */
     private String description;
 
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
+    @TableField(fill = FieldFill.UPDATE)
     private Date updateTime;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private String updateUser;
 }
