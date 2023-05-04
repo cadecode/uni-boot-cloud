de8a710228ebecb746dfb46524503b92ed002e5a
Cade Li <cade.li@qq.com>
2023-05-04 16:10:48 +0800

    feat: 添加角色绑定关系增删接口

--- application/src/main/java/top/cadecode/uniboot/controller/system/SysRoleController.java
+++ application/src/main/java/top/cadecode/uniboot/controller/system/SysRoleController.java
@@ -6,14 +6,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.cadecode.uniboot.common.annotation.ApiFormat;
 import top.cadecode.uniboot.system.bean.po.SysRole;
 import top.cadecode.uniboot.system.bean.vo.SysRoleVo.SysRoleListVo;
 import top.cadecode.uniboot.system.convert.SysRoleConvert;
+import top.cadecode.uniboot.system.request.SysRoleRequest.SysRoleMappingRequest;
 import top.cadecode.uniboot.system.service.SysRoleService;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 /**
@@ -38,5 +41,41 @@ public class SysRoleController {
     public List<SysRoleListVo> roleList() {
         List<SysRole> roleList = sysRoleService.list();
         return SysRoleConvert.INSTANCE.poToListVo(roleList);
+    }
+
+    @ApiOperation("添加用户角色绑定")
+    @PostMapping("add_user_mapping")
+    public boolean addUserMapping(@RequestBody @NotEmpty List<SysRoleMappingRequest> request) {
+        return sysRoleService.addRoleUser(request) > 0;
+    }
+
+    @ApiOperation("删除用户角色绑定")
+    @PostMapping("remove_user_mapping")
+    public boolean removeUserMapping(@RequestBody @NotEmpty List<SysRoleMappingRequest> request) {
+        return sysRoleService.removeRoleUser(request) > 0;
+    }
+
+    @ApiOperation("添加菜单角色绑定")
+    @PostMapping("add_menu_mapping")
+    public boolean addMenuMapping(@RequestBody @NotEmpty List<SysRoleMappingRequest> request) {
+        return sysRoleService.addRoleMenu(request) > 0;
+    }
+
+    @ApiOperation("删除菜单角色绑定")
+    @PostMapping("remove_menu_mapping")
+    public boolean removeMenuMapping(@RequestBody @NotEmpty List<SysRoleMappingRequest> request) {
+        return sysRoleService.removeRoleMenu(request) > 0;
+    }
+
+    @ApiOperation("添加API角色绑定")
+    @PostMapping("add_api_mapping")
+    public boolean addApiMapping(@RequestBody @NotEmpty List<SysRoleMappingRequest> request) {
+        return sysRoleService.addRoleApi(request) > 0;
+    }
+
+    @ApiOperation("删除API角色绑定")
+    @PostMapping("remove_api_mapping")
+    public boolean removeApiMapping(@RequestBody @NotEmpty List<SysRoleMappingRequest> request) {
+        return sysRoleService.removeRoleApi(request) > 0;
     }
 }
