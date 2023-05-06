55d81bf367f18585a6b7030420868d0b534232ee
Cade Li <cadecode@foxmail.com>
2023-05-06 12:50:10 +0800

    feat: 添加menu管理接口

--- system/src/main/java/top/cadecode/uniboot/system/bean/vo/SysMenuVo.java
+++ system/src/main/java/top/cadecode/uniboot/system/bean/vo/SysMenuVo.java
@@ -3,6 +3,7 @@ package top.cadecode.uniboot.system.bean.vo;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,5 +33,36 @@ public class SysMenuVo {
         private String icon;
 
         private List<SysMenuTreeVo> children = new ArrayList<>();
+    }
+
+    @Data
+    public static class SysMenuRolesVo {
+        private Long id;
+
+        private Long parentId;
+
+        private String routeName;
+
+        private String routePath;
+
+        private String componentPath;
+
+        private String menuName;
+
+        private Boolean leafFlag;
+
+        private String icon;
+
+        private Integer orderNum;
+
+        private Boolean enableFlag;
+
+        private List<String> roles;
+
+        private Date createTime;
+
+        private Date updateTime;
+
+        private String updateUser;
     }
 }
--- system/src/main/java/top/cadecode/uniboot/system/mapper/SysMenuMapper.java
+++ system/src/main/java/top/cadecode/uniboot/system/mapper/SysMenuMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import top.cadecode.uniboot.system.bean.po.SysMenu;
+import top.cadecode.uniboot.system.bean.vo.SysMenuVo.SysMenuRolesVo;
+import top.cadecode.uniboot.system.request.SysMenuRequest.SysMenuRolesRequest;
 
 import java.util.List;
 
@@ -17,4 +19,9 @@ import java.util.List;
 public interface SysMenuMapper extends BaseMapper<SysMenu> {
 
     List<SysMenu> selectByRoles(@Param("roleCodes") List<String> roleCodes);
+
+    List<SysMenuRolesVo> selectRolesVo(@Param("request") SysMenuRolesRequest request);
+
+    List<SysMenuRolesVo> selectRolesVoByMenuIds(@Param("menuIds") List<Long> menuIds);
+
 }
