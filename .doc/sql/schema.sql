9d6806f18aca61dcfda79576830d0977752e7c04
Cade Li <cade.li@qq.com>
2023-05-27 12:16:34 +0800

    docs: 添加 sys_log create sql

--- .doc/sql/schema.sql
+++ .doc/sql/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS sys_user
     PRIMARY KEY (id),
     UNIQUE KEY uk_username (username)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '系统用户表';
 -- ----------------------------
 -- 系统角色表
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS sys_role
     PRIMARY KEY (id),
     UNIQUE KEY uk_code (code)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '系统角色表';
 -- ----------------------------
 -- 系统菜单表
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS sys_menu
     PRIMARY KEY (id),
     UNIQUE KEY uk_route_name (route_name)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '系统菜单表';
 -- ----------------------------
 -- 系统权限表
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS sys_api
     PRIMARY KEY (id),
     UNIQUE KEY uk_url (url)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '系统权限表';
 -- ----------------------------
 -- 角色用户关系表
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS sys_role_user
     PRIMARY KEY (role_id, user_id),
     INDEX idx_user_id (user_id)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '角色用户关系表';
 -- ----------------------------
 -- 角色权限关系表
@@ -103,7 +103,7 @@ CREATE TABLE IF NOT EXISTS sys_role_api
     PRIMARY KEY (role_id, api_id),
     INDEX idx_api_id (api_id)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '角色权限关系表';
 -- ----------------------------
 -- 角色菜单关系表
@@ -115,5 +115,39 @@ CREATE TABLE IF NOT EXISTS sys_role_menu
     PRIMARY KEY (role_id, menu_id),
     INDEX idx_api_id (menu_id)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
+  DEFAULT CHARSET = utf8mb4
     COMMENT '角色菜单关系表';
+-- ----------------------------
+-- 系统日志表
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS sys_log
+(
+    id             BIGINT UNSIGNED,
+    log_type       VARCHAR(50)   NOT NULL COMMENT 'log 类型',
+    url            VARCHAR(500)  NULL COMMENT 'URL',
+    exceptional    TINYINT(1)    NOT NULL COMMENT '是否异常',
+    access_user    VARCHAR(100)  NULL COMMENT '访问者',
+    description    VARCHAR(1000) NULL COMMENT '描述',
+    class_method   VARCHAR(500)  NOT NULL COMMENT '方法名',
+    thread_id      VARCHAR(100)  NULL COMMENT '线程 ID',
+    thread_name    VARCHAR(500)  NULL COMMENT '线程名',
+    ip             VARCHAR(50)   NULL COMMENT 'IP',
+    http_method    VARCHAR(50)   NULL COMMENT 'HTTP 方法',
+    request_params TEXT          NULL COMMENT '参数',
+    result         TEXT          NULL COMMENT '结果',
+    time_cost      BIGINT        NULL COMMENT '接口耗时',
+    os             VARCHAR(200)  NULL COMMENT '操作系统',
+    browser        VARCHAR(200)  NULL COMMENT '浏览器',
+    user_agent     VARCHAR(500)  NULL COMMENT 'user-agent',
+
+    create_time    DATETIME DEFAULT CURRENT_TIMESTAMP,
+    update_time    DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    update_user    VARCHAR(100)  NULL,
+    PRIMARY KEY (id),
+    INDEX idx_create_time (create_time),
+    INDEX idx_log_type (log_type),
+    INDEX idx_url (url),
+    INDEX idx_access_user (access_user)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+    COMMENT '系统日志表';
