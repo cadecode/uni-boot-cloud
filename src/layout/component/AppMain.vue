bbf06eda87f6d17074c13047a166577996f418b4
Cade Li <cade.li@qq.com>
2023-05-29 16:10:11 +0800

    feat: 修改 app-container 全局页面根元素样式

--- src/layout/component/AppMain.vue
+++ src/layout/component/AppMain.vue
@@ -17,8 +17,7 @@ export default {
 </script>
 <style scoped>
 .app-main {
-  /*50 = navbar  */
-  min-height: calc(100vh - 50px);
+  min-height: 100vh;
   width: 100%;
   position: relative;
   overflow: hidden;
