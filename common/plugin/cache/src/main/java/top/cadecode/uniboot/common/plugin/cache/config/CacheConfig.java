1bd0a786df2ee7f8cba2df452bfff84a7c83a3c5
Cade Li <cade.li@qq.com>
2023-06-09 23:58:20 +0800

    feat: 添加 RedisCacheManager 配置，修改 KeyPrefix

--- common/plugin/cache/src/main/java/top/cadecode/uniboot/common/plugin/cache/config/CacheConfig.java
+++ common/plugin/cache/src/main/java/top/cadecode/uniboot/common/plugin/cache/config/CacheConfig.java
@@ -7,7 +7,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -35,5 +41,39 @@ public class CacheConfig {
                 .expireAfterWrite(5, TimeUnit.SECONDS));
         caffeineCacheManager.setAllowNullValues(true);
         return caffeineCacheManager;
+    }
+
+    /**
+     * Redis 缓存
+     * 过期时间 5 分钟
+     *
+     * @return RedisCacheManager 实例
+     */
+    @Bean
+    public CacheManager redis5m(RedisTemplate<String, ?> jsonRedisTemplate) {
+        return geneRedisCacheManager(jsonRedisTemplate, 5);
+    }
+
+    /**
+     * Redis 缓存
+     * 过期时间 30 分钟
+     *
+     * @return RedisCacheManager 实例
+     */
+    @Primary
+    @Bean
+    public CacheManager redis30m(RedisTemplate<String, ?> jsonRedisTemplate) {
+        return geneRedisCacheManager(jsonRedisTemplate, 30);
+    }
+
+    private static RedisCacheManager geneRedisCacheManager(RedisTemplate<String, ?> jsonRedisTemplate, long minutes) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(SerializationPair.fromSerializer(jsonRedisTemplate.getStringSerializer()))
+                .serializeValuesWith(SerializationPair.fromSerializer(jsonRedisTemplate.getValueSerializer()))
+                .entryTtl(Duration.ofMinutes(minutes));
+        return RedisCacheManager.builder(jsonRedisTemplate.getConnectionFactory())
+                .cacheDefaults(cacheConfiguration)
+                .transactionAware()
+                .build();
     }
 }
