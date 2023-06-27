3bfe871f73410fde273d98e7a040f3de8d091ee4
Cade Li <cade.li@qq.com>
2023-06-27 09:31:13 +0800

    feat: JacksonUtil 添加带序列化类型的工具方法

--- common/core/src/main/java/com/github/cadecode/uniboot/common/core/util/JacksonUtil.java
+++ common/core/src/main/java/com/github/cadecode/uniboot/common/core/util/JacksonUtil.java
@@ -2,10 +2,12 @@ package com.github.cadecode.uniboot.common.core.util;
 
 import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.cadecode.uniboot.common.core.exception.UtilException;
 import lombok.extern.slf4j.Slf4j;
@@ -27,8 +29,16 @@ import java.util.Objects;
 @Component
 public class JacksonUtil implements InitializingBean {
 
-    private static ObjectMapper OBJECT_MAPPER;
+    private static ObjectMapper PROJECT_OBJECT_MAPPER;
 
+    /**
+     * ObjectMapper，序列化结果中携带类型
+     */
+    private static ObjectMapper TYPING_OBJECT_MAPPER;
+
+    /**
+     * SpringBoot 项目默认 ObjectMapper Bean
+     */
     private ObjectMapper objectMapper;
 
     /**
@@ -40,7 +50,14 @@ public class JacksonUtil implements InitializingBean {
     }
 
     public static ObjectMapper getMapper() {
-        return OBJECT_MAPPER;
+        return getMapper(false);
+    }
+
+    public static ObjectMapper getMapper(boolean typing) {
+        if (typing) {
+            return TYPING_OBJECT_MAPPER;
+        }
+        return PROJECT_OBJECT_MAPPER;
     }
 
     /**
@@ -50,7 +67,7 @@ public class JacksonUtil implements InitializingBean {
      * @return json 字符串
      */
     public static String toJson(Object bean) {
-        return toJson(bean, false);
+        return toJson(bean, false, false);
     }
 
     /**
@@ -61,6 +78,18 @@ public class JacksonUtil implements InitializingBean {
      * @return json 字符串
      */
     public static String toJson(Object bean, boolean isPretty) {
+        return toJson(bean, false, isPretty);
+    }
+
+    /**
+     * 转换对象到 json 串
+     *
+     * @param bean     需要转换的对象
+     * @param typing   是否序列化类型
+     * @param isPretty 是否美化输出（换行）
+     * @return json 字符串
+     */
+    public static String toJson(Object bean, boolean typing, boolean isPretty) {
         if (bean == null) {
             return null;
         }
@@ -69,23 +98,56 @@ public class JacksonUtil implements InitializingBean {
         }
         try {
             if (!isPretty) {
-                return OBJECT_MAPPER.writeValueAsString(bean);
+                return getMapper(typing).writeValueAsString(bean);
             }
-            return OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(bean);
+            return getMapper(typing).writerWithDefaultPrettyPrinter().writeValueAsString(bean);
         } catch (JsonProcessingException e) {
-            throw new UtilException("cast bean to json fail", e);
+            throw new UtilException("Cast bean to json fail", e);
         }
     }
 
     /**
-     * 转换字符串到自定义对象
+     * 转换字符串到自定义对象，指定 class
      *
      * @param json  需要转换的字符串
      * @param clazz 自定义对象的 class 对象
      * @return 自定义对象
      */
-    @SuppressWarnings("unchecked")
     public static <T> T toBean(String json, Class<T> clazz) {
+        return toBean(json, false, clazz);
+    }
+
+    /**
+     * 转换字符串到集合对象，指定泛型类
+     *
+     * @param json          需要转换的字符串
+     * @param typeReference 自定义对象的 TypeReference 对象
+     * @return 自定义对象
+     */
+    public static <T> T toBean(String json, TypeReference<T> typeReference) {
+        return toBean(json, false, typeReference);
+    }
+
+    /**
+     * 转换字符串到自定义对象，序列化类型
+     *
+     * @param json 需要转换的字符串
+     * @return 自定义对象
+     */
+    public static Object toBean(String json) {
+        return toBean(json, true, Object.class);
+    }
+
+    /**
+     * 转换字符串到自定义对象
+     *
+     * @param json   需要转换的字符串
+     * @param typing 是否序列化类型
+     * @param clazz  自定义对象的 class 对象
+     * @return 自定义对象
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T toBean(String json, boolean typing, Class<T> clazz) {
         if (StrUtil.isEmpty(json) || Objects.isNull(clazz)) {
             return null;
         }
@@ -93,9 +155,9 @@ public class JacksonUtil implements InitializingBean {
             return (T) json;
         }
         try {
-            return OBJECT_MAPPER.readValue(json, clazz);
+            return getMapper(typing).readValue(json, clazz);
         } catch (Exception e) {
-            throw new UtilException("cast json to bean fail", e);
+            throw new UtilException("Cast json to bean fail", e);
         }
     }
 
@@ -103,11 +165,12 @@ public class JacksonUtil implements InitializingBean {
      * 转换字符串到集合对象，可以保留泛型
      *
      * @param json          需要转换的字符串
+     * @param typing        是否序列化类型
      * @param typeReference 自定义对象的 TypeReference 对象
      * @return 自定义对象
      */
     @SuppressWarnings("unchecked")
-    public static <T> T toBean(String json, TypeReference<T> typeReference) {
+    public static <T> T toBean(String json, boolean typing, TypeReference<T> typeReference) {
         if (StrUtil.isEmpty(json) || Objects.isNull(typeReference)) {
             return null;
         }
@@ -115,31 +178,34 @@ public class JacksonUtil implements InitializingBean {
             return (T) json;
         }
         try {
-            return OBJECT_MAPPER.readValue(json, typeReference);
+            return getMapper(typing).readValue(json, typeReference);
         } catch (IOException e) {
-            throw new UtilException("cast json to bean fail", e);
+            throw new UtilException("Cast json to bean fail", e);
         }
     }
 
     @Override
     public void afterPropertiesSet() {
-        OBJECT_MAPPER = this.objectMapper;
-        if (Objects.isNull(OBJECT_MAPPER)) {
+        PROJECT_OBJECT_MAPPER = objectMapper;
+        if (Objects.isNull(PROJECT_OBJECT_MAPPER)) {
             log.warn("Bean objectMapper not found, use default config by JacksonUtil");
-            OBJECT_MAPPER = new ObjectMapper();
+            PROJECT_OBJECT_MAPPER = new ObjectMapper();
             // 定义日期格式
             String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
-            // Jackson 详细
+            // Jackson 详细配置
             // 列入对象的全部字段
-            OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+            PROJECT_OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.ALWAYS);
             // 取消默认转换 timestamps 形式
-            OBJECT_MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            PROJECT_OBJECT_MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
             // 忽略空Bean转json的错误
-            OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            PROJECT_OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
             // 统一所有的日期格式为以下的样式，即 yyyy-MM-dd HH:mm:ss
-            OBJECT_MAPPER.setDateFormat(new SimpleDateFormat(STANDARD_FORMAT));
+            PROJECT_OBJECT_MAPPER.setDateFormat(new SimpleDateFormat(STANDARD_FORMAT));
             // 忽略在 json 字符串中存在，但是在 java 对象中不存在对应属性的情况。防止错误
-            OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            PROJECT_OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         }
+        // 启用序列化类型
+        TYPING_OBJECT_MAPPER = PROJECT_OBJECT_MAPPER.copy();
+        TYPING_OBJECT_MAPPER.activateDefaultTyping(TYPING_OBJECT_MAPPER.getPolymorphicTypeValidator(), DefaultTyping.NON_FINAL, As.PROPERTY);
     }
 }
