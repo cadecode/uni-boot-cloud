ac27c24a9c7a5588cceb1dce8e270e56dfc7afdd
Cade Li <cadecode@foxmail.com>
2023-07-27 16:33:53 +0800

    feat: 引入 nacos、openFeign

--- framework/framework_svc/src/main/java/com/github/cadecode/uniboot/framework/svc/UniFrameworkApplication.java
+++ framework/framework_svc/src/main/java/com/github/cadecode/uniboot/framework/svc/UniFrameworkApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * 启动类
  */
 @MapperScan("com.github.cadecode.**.mapper")
+@EnableFeignClients("com.github.cadecode")
+@EnableDiscoveryClient
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class UniFrameworkApplication extends SpringBootServletInitializer {
