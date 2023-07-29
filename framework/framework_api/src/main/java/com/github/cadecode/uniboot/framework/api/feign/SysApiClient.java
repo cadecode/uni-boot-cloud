e0e6c1f0271aea52c591f5b768a449af0bf8f11b
Cade Li <cade.li@qq.com>
2023-07-29 20:40:22 +0800

    feat: 添加 const SvcName 维护服务名

--- framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/feign/SysApiClient.java
+++ framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/feign/SysApiClient.java
@@ -2,6 +2,7 @@ package com.github.cadecode.uniboot.framework.api.feign;
 
 import com.github.cadecode.uniboot.framework.api.bean.vo.SysApiVo.SysApiRolesVo;
 import com.github.cadecode.uniboot.framework.api.consts.KeyPrefix;
+import com.github.cadecode.uniboot.framework.api.consts.SvcName;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import java.util.List;
  * @author Cade Li
  * @since 2023/7/28
  */
-@FeignClient(contextId = "SysApiClient", name = "uni-boot-framework")
+@FeignClient(contextId = "SysApiClient", name = SvcName.FRAMEWORK)
 public interface SysApiClient {
 
     @Cacheable(cacheNames = KeyPrefix.API_ROLES, key = "'all'")
