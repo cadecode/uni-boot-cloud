39f27dbc9aaf755644ce5979a228ef6705abd936
Cade Li <cade.li@qq.com>
2023-07-31 16:18:06 +0800

    refactor: 类名修改

--- framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/plugin/MybatisFillHandler.java
+++ framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/plugin/MybatisFillHandler.java
@@ -6,13 +6,13 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
 /**
- * 对象填充处理器
+ * mybatis plus 对象填充处理器
  *
  * @author Cade Li
  * @since 2023/6/8
  */
 @Component
-public class ObjectFillHandler extends AbstractObjectFillHandler {
+public class MybatisFillHandler extends AbstractObjectFillHandler {
     @Override
     public void updateUser(MetaObject metaObject) {
         this.setFieldValByName("updateUser", SecurityUtil.getUsername(), metaObject);
