ec626b6296ed8305326a48e11bc0c4e6703d5dd3
Cade Li <cade.li@qq.com>
2023-08-01 10:41:03 +0800

    feat: 定义 feign client 装饰器，实现请求与响应的拦截处理

--- framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/config/FeignConfig.java
+++ framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/config/FeignConfig.java
@@ -1,21 +1,17 @@
 package com.github.cadecode.uniboot.framework.api.config;
 
-import cn.hutool.core.util.CharsetUtil;
-import cn.hutool.core.util.EscapeUtil;
-import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.extra.servlet.ServletUtil;
-import com.github.cadecode.uniboot.common.core.util.JacksonUtil;
-import com.github.cadecode.uniboot.framework.api.bean.dto.SysUserDto.SysUserDetailsDto;
-import com.github.cadecode.uniboot.framework.api.consts.SecurityConst;
-import com.github.cadecode.uniboot.framework.api.util.SecurityUtil;
-import feign.RequestInterceptor;
+import com.github.cadecode.uniboot.framework.api.feign.FeignClientDecorator;
+import feign.Client;
+import feign.Client.Default;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.loadbalancer.LoadBalancedRetryFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.cloud.openfeign.loadbalancer.FeignBlockingLoadBalancerClient;
+import org.springframework.cloud.openfeign.loadbalancer.RetryableFeignBlockingLoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * OpenFeign 配置类
@@ -26,33 +22,23 @@ import javax.servlet.http.HttpServletRequest;
 @Configuration
 public class FeignConfig {
 
+    // 覆盖 feign 配置
+    // org.springframework.cloud.openfeign.loadbalancer.DefaultFeignLoadBalancerConfiguration
+
     @Bean
-    public RequestInterceptor requestInterceptor() {
-        return requestTemplate -> {
-            // 获取请求对象
-            ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-            // 非请求接口内的调用
-            if (ObjectUtil.isNull(requestAttributes)) {
-                return;
-            }
-            HttpServletRequest sRequest = requestAttributes.getRequest();
-            if (ObjectUtil.isNull(sRequest)) {
-                return;
-            }
-            // 传递用户 token
-            String token = SecurityUtil.getTokenFromRequest(sRequest);
-            if (StrUtil.isNotEmpty(token)) {
-                requestTemplate.header(SecurityConst.HEAD_TOKEN, token);
-            }
-            // 传递用户详细信息
-            String userDetailsJson = ServletUtil.getHeader(sRequest, SecurityConst.HEAD_USER_DETAILS, CharsetUtil.CHARSET_UTF_8);
-            if (StrUtil.isEmpty(userDetailsJson)) {
-                SysUserDetailsDto userDetailsDto = SecurityUtil.getUserDetails(null);
-                userDetailsJson = JacksonUtil.toJson(userDetailsDto);
-            }
-            requestTemplate.header(SecurityConst.HEAD_USER_DETAILS, EscapeUtil.escape(userDetailsJson));
-            // 配置客户端 IP
-            requestTemplate.header("X-Forwarded-For", ServletUtil.getClientIP(sRequest));
-        };
+    @ConditionalOnProperty(value = "spring.cloud.loadbalancer.retry.enabled", havingValue = "false")
+    public Client feignClient(LoadBalancerClient loadBalancerClient, LoadBalancerProperties properties,
+                              LoadBalancerClientFactory loadBalancerClientFactory) {
+        FeignClientDecorator decorator = new FeignClientDecorator(new Default(null, null));
+        return new FeignBlockingLoadBalancerClient(decorator, loadBalancerClient, properties, loadBalancerClientFactory);
     }
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.cloud.loadbalancer.retry.enabled", havingValue = "true", matchIfMissing = true)
+    public Client feignRetryClient(LoadBalancerClient loadBalancerClient, LoadBalancedRetryFactory loadBalancedRetryFactory,
+                                   LoadBalancerProperties properties, LoadBalancerClientFactory loadBalancerClientFactory) {
+        FeignClientDecorator decorator = new FeignClientDecorator(new Default(null, null));
+        return new RetryableFeignBlockingLoadBalancerClient(decorator, loadBalancerClient, loadBalancedRetryFactory, properties, loadBalancerClientFactory);
+    }
+
 }
