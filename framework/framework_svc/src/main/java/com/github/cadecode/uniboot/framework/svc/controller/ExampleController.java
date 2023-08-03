0a45528e389428a18d9cc0645a6a74ace26fa297
Cade Li <cadecode@foxmail.com>
2023-08-03 17:07:03 +0800

    feat: 添加几个测试接口

--- framework/framework_svc/src/main/java/com/github/cadecode/uniboot/framework/svc/controller/ExampleController.java
+++ framework/framework_svc/src/main/java/com/github/cadecode/uniboot/framework/svc/controller/ExampleController.java
@@ -1,6 +1,9 @@
 package com.github.cadecode.uniboot.framework.svc.controller;
 
+import com.github.cadecode.uniboot.common.core.exception.ApiException;
+import com.github.cadecode.uniboot.common.core.web.response.ApiResult;
 import com.github.cadecode.uniboot.framework.api.annotation.ApiFormat;
+import com.github.cadecode.uniboot.framework.api.enums.FrameErrorEnum;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -24,12 +27,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("example")
 public class ExampleController {
 
-    // For feign client
-    @ApiFormat(false)
     @ApiOperation("传递字符串")
     @PostMapping("test_str")
     public String testStr(@RequestParam String str) {
         return str;
     }
 
+    @ApiOperation("返回异常结果")
+    @PostMapping("test_result")
+    public ApiResult<?> testApiResult() {
+        return ApiResult.error(FrameErrorEnum.UNKNOWN).moreInfo("测试返回异常结果");
+    }
+
+    @ApiOperation("直接抛出异常")
+    @PostMapping("test_exception")
+    public Object testException() {
+        throw ApiException.of("测试直接抛出异常");
+    }
 }
