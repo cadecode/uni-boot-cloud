fd1a7d31e1f3410ea3c623bf51a47d75199b83bd
Cade Li <cade.li@qq.com>
2023-08-08 15:27:12 +0800

    feat: 封装判断是否内部调用的方法

--- framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/util/RequestUtil.java
+++ framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/util/RequestUtil.java
@@ -1,6 +1,12 @@
 package com.github.cadecode.uniboot.framework.api.util;
 
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.EscapeUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+import com.github.cadecode.uniboot.common.core.util.JacksonUtil;
+import com.github.cadecode.uniboot.framework.api.bean.dto.SysUserDto.SysUserDetailsDto;
+import com.github.cadecode.uniboot.framework.api.consts.SecurityConst;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -29,5 +35,43 @@ public class RequestUtil {
             return null;
         }
         return requestAttributes.getResponse();
+    }
+
+    /**
+     * 根据请求头判断是否是内部请求
+     *
+     * @param request 请求对象，为 null 时自动从上下文获取
+     * @return 是否是内部请求
+     */
+    public static boolean isInnerRequest(HttpServletRequest request) {
+        if (ObjectUtil.isNull(request)) {
+            request = getRequest();
+        }
+        if (ObjectUtil.isNull(request)) {
+            return false;
+        }
+        String source = ServletUtil.getHeader(request, SecurityConst.HEAD_SOURCE, CharsetUtil.CHARSET_UTF_8);
+        return ObjectUtil.equal(source, SecurityConst.HEAD_SOURCE_VALUE);
+    }
+
+    /**
+     * 根据请求头获取内部请求携带的用户信息
+     *
+     * @param request 请求对象，为 null 时自动从上下文获取
+     * @return 内部请求携带的用户信息
+     */
+    public static SysUserDetailsDto getInnerUserDetails(HttpServletRequest request) {
+        if (ObjectUtil.isNull(request)) {
+            request = getRequest();
+        }
+        if (ObjectUtil.isNull(request)) {
+            return null;
+        }
+        // feign 拦截器填充时做了转义处理
+        String escapedUserDetailsJson = ServletUtil.getHeader(request, SecurityConst.HEAD_USER_DETAILS, CharsetUtil.CHARSET_UTF_8);
+        if (ObjectUtil.isNotEmpty(escapedUserDetailsJson)) {
+            return JacksonUtil.toBean(EscapeUtil.unescape(escapedUserDetailsJson), SysUserDetailsDto.class);
+        }
+        return null;
     }
 }
