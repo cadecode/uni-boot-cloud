86feeef9db49ae4ecc3607be11d6606beda5a910
Cade Li <cade.li@qq.com>
2023-08-11 00:00:06 +0800

    feat: gateway 添加 filter 生成 traceId

--- gateway/src/main/java/com/github/cadecode/uniboot/gateway/filter/TraceInfoFilter.java
+++ gateway/src/main/java/com/github/cadecode/uniboot/gateway/filter/TraceInfoFilter.java
@@ -1,5 +1,7 @@
 package com.github.cadecode.uniboot.gateway.filter;
 
+import cn.hutool.core.lang.UUID;
+import org.slf4j.MDC;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
@@ -9,28 +11,27 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /**
- * 请求头统一过滤
+ * 用于添加 trace id 的过滤器
  *
  * @author Cade Li
- * @since 2023/8/8
+ * @since 2023/8/10
  */
-@Order(1)
+@Order(0)
 @Component
-public class RequestHeaderFilter implements GlobalFilter {
+public class TraceInfoFilter implements GlobalFilter {
 
-    public static final String HEAD_SOURCE = "from-source";
-
-    public static final String HEAD_USER_DETAILS = "inner-user-details";
+    public static final String HEAD_TRACE_ID = "trace-id";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // 去掉内部调用专用的请求头，防止伪造
+        String traceId = UUID.fastUUID().toString(true);
         ServerHttpRequest request = exchange.getRequest().mutate()
                 .headers(h -> {
-                    h.remove(HEAD_SOURCE);
-                    h.remove(HEAD_USER_DETAILS);
+                    MDC.put(HEAD_TRACE_ID, traceId);
+                    h.set(HEAD_TRACE_ID, traceId);
                 })
                 .build();
+        exchange.getResponse().getHeaders().set(HEAD_TRACE_ID, traceId);
         return chain.filter(exchange.mutate().request(request).build());
     }
 }
