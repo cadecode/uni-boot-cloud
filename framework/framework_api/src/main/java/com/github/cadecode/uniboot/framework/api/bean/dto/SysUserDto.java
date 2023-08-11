dd448a8047314a6be89565af41d802384a80222e
Cade Li <cadecode@foxmail.com>
2023-08-11 14:04:30 +0800

    feat: 后缀 dto 改为 vo

--- framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/bean/dto/SysUserDto.java
+++ framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/bean/dto/SysUserDto.java
@@ -1,11 +1,7 @@
 package com.github.cadecode.uniboot.framework.api.bean.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.github.cadecode.uniboot.framework.api.bean.vo.SysMenuVo.SysMenuTreeVo;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -82,13 +78,5 @@ public class SysUserDto {
         public boolean isEnabled() {
             return true;
         }
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class SysUserInfoDto {
-        private List<SysMenuTreeVo> menuList;
     }
 }
