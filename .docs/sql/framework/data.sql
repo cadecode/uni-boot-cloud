ab38fe2c632efb5f8660b401da34fde37bdcfa1f
Cade Li <cade.li@qq.com>
2023-08-18 16:47:10 +0800

    docs: 日志表脚本维护到通用 SQL，日志管理 菜单放到 开发管理 下

--- .docs/sql/framework/data.sql
+++ .docs/sql/framework/data.sql
@@ -24,49 +24,49 @@ INSERT INTO sys_role (id, code, name, description)
 -- ----------------------------
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (1, null, 'Home', '/home/demo_home', '/Home/DemoHome', '首页', 1, 0, 1);
+    VALUE (1, null, 'Home', '/home/demo_home', '/Home/DemoHome', '首页', 1, 1, 1);
 
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (2, null, 'System', '/system', null, '系统管理', 0, 1, 1);
+    VALUE (2, null, 'System', '/system', null, '系统管理', 0, 2, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (3, 2, 'User', '/system/user', '/System/User', '用户管理', 1, 101, 1);
+    VALUE (201, 2, 'User', '/system/user', '/System/User', '用户管理', 1, 201, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (4, 2, 'Role', '/system/role', '/System/Role', '角色管理', 1, 102, 1);
+    VALUE (202, 2, 'Role', '/system/role', '/System/Role', '角色管理', 1, 202, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (5, 2, 'Menu', '/system/menu', '/System/Menu', '菜单管理', 1, 103, 1);
+    VALUE (203, 2, 'Menu', '/system/menu', '/System/Menu', '菜单管理', 1, 203, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (6, 2, 'Api', '/system/api', '/System/Api', 'API管理', 1, 104, 1);
+    VALUE (204, 2, 'Api', '/system/api', '/System/Api', 'API管理', 1, 204, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (7, 2, 'Log', '/system/log', '/System/Log', '日志管理', 1, 105, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (8, 2, 'Dict', '/system/dict', '/System/Dict', '字典管理', 1, 106, 1);
+    VALUE (205, 2, 'Dict', '/system/dict', '/System/Dict', '字典管理', 1, 205, 1);
 
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (9, null, 'Develop', '/develop', null, '开发管理', 0, 2, 1);
+    VALUE (3, null, 'Develop', '/develop', null, '开发管理', 0, 2, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (10, 9, 'Icon', '/develop/icon', '/Develop/Icon', '图标', 1, 201, 1);
+    VALUE (301, 3, 'Log', '/develop/log', '/Develop/Log', '日志管理', 1, 301, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (11, 9, 'Echarts', '/develop/echarts', '/Develop/Echarts', 'Echarts 示例', 1, 202, 1);
+    VALUE (306, 3, 'Icon', '/develop/icon', '/Develop/Icon', '图标', 1, 306, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (12, 9, 'Swagger', 'http://localhost:8000/doc.html', null, 'Swagger', 1, 203, 1);
+    VALUE (307, 3, 'Echarts', '/develop/echarts', '/Develop/Echarts', 'Echarts 示例', 1, 307, 1);
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (13, 9, 'Druid', 'http://localhost:8000/druid', null, 'Druid', 1, 204, 1);
+    VALUE (308, 3, 'Swagger', 'http://localhost:8000/doc.html', null, 'Swagger', 1, 308, 1);
+INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
+                     enable_flag)
+    VALUE (309, 3, 'Druid', 'http://localhost:8000/druid', null, 'Druid', 1, 309, 1);
 
 INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
                      enable_flag)
-    VALUE (100, null, 'GithubUrl', 'https://github.com/cadecode/uni-boot-admin', null, '源码仓库', 1, 100, 1);
+    VALUE (1000, null, 'GithubUrl', 'https://github.com/cadecode/uni-boot-admin', null, '源码仓库', 1, 1000, 1);
 -- ----------------------------
 -- 插入 API
 -- ----------------------------
@@ -89,53 +89,51 @@ VALUES (1, 1);
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (1, 2);
 INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 201);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 202);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 203);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 204);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 205);
+INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (1, 3);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 4);
+VALUES (1, 301);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 5);
+VALUES (1, 306);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 6);
+VALUES (1, 307);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 7);
+VALUES (1, 308);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 8);
+VALUES (1, 309);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 9);
-INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 10);
-INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 11);
-INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 12);
-INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 13);
-INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (1, 100);
+VALUES (1, 1000);
 
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (2, 1);
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (2, 2);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 3);
+VALUES (2, 201);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 4);
+VALUES (2, 202);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 5);
+VALUES (2, 203);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 6);
+VALUES (2, 204);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 7);
+VALUES (2, 205);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 8);
-INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (2, 100);
+VALUES (2, 1000);
 
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (3, 1);
 INSERT INTO sys_role_menu (role_id, menu_id)
-VALUES (3, 100);
+VALUES (3, 1000);
 -- ----------------------------
 -- 插入角色API关系
 -- ----------------------------
@@ -150,41 +148,41 @@ VALUES (3, 1);
 -- ----------------------------
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667915699808002049, '日志类型', 'logType', '查询', 'Query', '日志类型-查询', 0, 0, '2023-06-11 23:24:26',
+VALUES (1, '日志类型', 'logType', '查询', 'Query', '日志类型-查询', 0, 0, '2023-06-11 23:24:26',
         '2023-06-12 00:11:57', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667915811166773250, '日志类型', 'logType', '删除', 'Remove', '日志类型-删除', 1, 0, '2023-06-11 23:24:52',
+VALUES (2, '日志类型', 'logType', '删除', 'Remove', '日志类型-删除', 1, 0, '2023-06-11 23:24:52',
         '2023-06-12 00:12:01', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667915876962820097, '日志类型', 'logType', '更新', 'Update', '日志类型-更新', 2, 0, '2023-06-11 23:25:08',
+VALUES (3, '日志类型', 'logType', '更新', 'Update', '日志类型-更新', 2, 0, '2023-06-11 23:25:08',
         '2023-06-12 00:12:05', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667915965307445249, '日志类型', 'logType', '添加', 'Add', '日志类型-添加', 3, 0, '2023-06-11 23:25:29',
+VALUES (4, '日志类型', 'logType', '添加', 'Add', '日志类型-添加', 3, 0, '2023-06-11 23:25:29',
         '2023-06-12 00:12:09', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667916031808135169, '日志类型', 'logType', '鉴权', 'Auth', '日志类型-鉴权', 4, 0, '2023-06-11 23:25:45',
+VALUES (5, '日志类型', 'logType', '鉴权', 'Auth', '日志类型-鉴权', 4, 0, '2023-06-11 23:25:45',
         '2023-06-12 00:12:15', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667916084467621889, '日志类型', 'logType', '导入', 'Import', '日志类型-导入', 5, 0, '2023-06-11 23:25:58',
+VALUES (6, '日志类型', 'logType', '导入', 'Import', '日志类型-导入', 5, 0, '2023-06-11 23:25:58',
         '2023-06-12 00:12:19', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667916134056878081, '日志类型', 'logType', '导出', 'Export', '日志类型-导出', 6, 0, '2023-06-11 23:26:09',
+VALUES (7, '日志类型', 'logType', '导出', 'Export', '日志类型-导出', 6, 0, '2023-06-11 23:26:09',
         '2023-06-12 00:12:30', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667916204034646018, '日志类型', 'logType', '上传', 'Upload', '日志类型-上传', 7, 0, '2023-06-11 23:26:26',
+VALUES (8, '日志类型', 'logType', '上传', 'Upload', '日志类型-上传', 7, 0, '2023-06-11 23:26:26',
         '2023-06-12 00:12:35', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667916260599029762, '日志类型', 'logType', '下载', 'Download', '日志类型-下载', 8, 0, '2023-06-11 23:26:40',
+VALUES (9, '日志类型', 'logType', '下载', 'Download', '日志类型-下载', 8, 0, '2023-06-11 23:26:40',
         '2023-06-12 00:12:40', 'admin');
 INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
                       update_user)
-VALUES (1667916298465206274, '日志类型', 'logType', '其他', 'Other', '日志类型-其他', 9, 0, '2023-06-11 23:26:49',
+VALUES (10, '日志类型', 'logType', '其他', 'Other', '日志类型-其他', 9, 0, '2023-06-11 23:26:49',
         '2023-06-12 00:52:14', 'admin');
