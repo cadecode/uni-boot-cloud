c0f749f765dcf2a7a27faa29b105c142430543fd
Cade Li <cadecode@foxmail.com>
2023-08-18 22:26:10 +0800

    feat: 添加 rabbit 常量

--- common/plugin/mq/src/main/java/com/github/cadecode/uniboot/common/plugin/mq/consts/RabbitConst.java
+++ common/plugin/mq/src/main/java/com/github/cadecode/uniboot/common/plugin/mq/consts/RabbitConst.java
@@ -8,9 +8,26 @@ package com.github.cadecode.uniboot.common.plugin.mq.consts;
  */
 public interface RabbitConst {
 
+    // 交换机类型
+
+    String EXC_TYPE_TOPIC = "topic";
+
+    String EXC_TYPE_DIRECT = "direct";
+
+    String EXC_TYPE_FANOUT = "fanout";
+
     /**
      * delay 交换机类型
      */
     String EXC_TYPE_DELAYED = "x-delayed-message";
 
+    // 交换机类型 --- end
+
+    // 交换机名称
+    /**
+     * 默认交换机
+     */
+    String EXC_DEFAULT = "";
+
+    // 交换机名称 --- end
 }
