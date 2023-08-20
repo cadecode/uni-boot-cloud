007c569eb52f0a894f218cdb5be762eb8eac5e67
Cade Li <cadecode@foxmail.com>
2023-08-20 10:23:34 +0800

    feat: example 添加测试 mq 发送 text json 类型消息的接口

--- example/example_svc/src/main/java/com/github/cadecode/uniboot/example/svc/consumer/RabbitExampleConsumer.java
+++ example/example_svc/src/main/java/com/github/cadecode/uniboot/example/svc/consumer/RabbitExampleConsumer.java
@@ -1,6 +1,7 @@
 package com.github.cadecode.uniboot.example.svc.consumer;
 
 import cn.hutool.core.util.ObjectUtil;
+import com.github.cadecode.uniboot.example.svc.bean.data.ExampleMsgDo;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -19,8 +20,23 @@ import java.io.IOException;
 @Component
 public class RabbitExampleConsumer {
 
-    @RabbitListener(queues = "demo-queue-0", id = "demo-queue-0", ackMode = "AUTO")
-    public void demoQueue0(String body, Message message, Channel channel) throws IOException {
+    @RabbitListener(queues = "example-delay-queue-0", id = "example-delay-queue-0", ackMode = "AUTO")
+    public void exampleDelayQueue0(String body, Message message, Channel channel) throws IOException {
+        log.info("Received msg:{}", body);
+    }
+
+    /**
+     * 测试 SpEL 获取 queue name
+     */
+    @RabbitListener(queues = "#{@'example-delay-queue-1'.name}", id = "example-delay-queue-1")
+    public void exampleDelayQueue1(String body, Message message, Channel channel) throws IOException {
+        log.info("Received msg:{}", body);
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
+
+
+    @RabbitListener(queues = "example-biz-queue-0", id = "example-biz-queue-0", ackMode = "AUTO")
+    public void exampleBizQueue0(String body, Message message, Channel channel) throws IOException {
         log.info("Received msg:{}", body);
         // 自动模式下，测试重试机制
         if (ObjectUtil.equal(body, "ERROR")) {
@@ -28,9 +44,12 @@ public class RabbitExampleConsumer {
         }
     }
 
-    @RabbitListener(queues = "#{@'demo-queue-1'.name}", id = "demo-queue-1")
-    public void demoQueue1(String body, Message message, Channel channel) throws IOException {
-        log.info("Received msg:{}", body);
-        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    /**
+     * 测试用对象获取消息
+     */
+    @RabbitListener(queues = "example-biz-queue-1", id = "example-biz-queue-1", ackMode = "AUTO")
+    public void exampleBizQueue1(ExampleMsgDo msgDo, Message message, Channel channel) throws IOException {
+        log.info("Received msg:{}", msgDo);
     }
+
 }
