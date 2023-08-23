14589a1ccde01ba3b5ee0a3e3c0064cc40aef803
Cade Li <cade.li@qq.com>
2023-08-23 23:18:24 +0800

    feat: 添加 mq 消息查询更新接口

--- framework/framework_base/src/main/java/com/github/cadecode/uniboot/framework/base/plugin/convert/PlgMqMsgConvert.java
+++ framework/framework_base/src/main/java/com/github/cadecode/uniboot/framework/base/plugin/convert/PlgMqMsgConvert.java
@@ -4,9 +4,13 @@ import com.github.cadecode.uniboot.common.plugin.mq.config.TxMsgProperties.MsgOp
 import com.github.cadecode.uniboot.common.plugin.mq.model.BaseTxMsg;
 import com.github.cadecode.uniboot.common.plugin.mq.model.TxMsg;
 import com.github.cadecode.uniboot.framework.base.plugin.bean.po.PlgMqMsg;
+import com.github.cadecode.uniboot.framework.base.plugin.bean.vo.PlgMqMsgVo.PlgMqMsgPageResVo;
+import com.github.cadecode.uniboot.framework.base.plugin.bean.vo.PlgMqMsgVo.PlgMqMsgUpdateReqVo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 /**
  * PlgMqMsg Bean Convert
@@ -33,4 +37,24 @@ public interface PlgMqMsgConvert {
 
     MsgOption poToMsgOption(PlgMqMsg po);
 
+    PlgMqMsgPageResVo poToPageResVo(PlgMqMsg po);
+
+    List<PlgMqMsgPageResVo> poToPageResVo(List<PlgMqMsg> poList);
+
+    @Mapping(target = "updateUser", ignore = true)
+    @Mapping(target = "updateTime", ignore = true)
+    @Mapping(target = "routingKey", ignore = true)
+    @Mapping(target = "nextRetryTime", ignore = true)
+    @Mapping(target = "message", ignore = true)
+    @Mapping(target = "maxRetryTimes", ignore = true)
+    @Mapping(target = "exchange", ignore = true)
+    @Mapping(target = "createTime", ignore = true)
+    @Mapping(target = "consumeState", ignore = true)
+    @Mapping(target = "cause", ignore = true)
+    @Mapping(target = "bizType", ignore = true)
+    @Mapping(target = "bizKey", ignore = true)
+    @Mapping(target = "backoffMultiplier", ignore = true)
+    @Mapping(target = "backoffMaxInterval", ignore = true)
+    @Mapping(target = "backoffInitInterval", ignore = true)
+    PlgMqMsg voToPo(PlgMqMsgUpdateReqVo reqVo);
 }
