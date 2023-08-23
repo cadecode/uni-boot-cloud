0af2ca0b22ac09bba323abad7a4ced2cb452e20e
Cade Li <cade.li@qq.com>
2023-08-23 23:16:05 +0800

    feat: 搜索按钮设为 primary

--- src/view/Develop/Log/index.vue
+++ src/view/Develop/Log/index.vue
@@ -72,7 +72,7 @@
           <el-table-column property="createTime" label="访问时间" width="150px" />
           <el-table-column label="操作" width="180px">
             <template slot-scope="scope">
-              <el-button size="mini" @click="() => showParamsDialog.showDialog = true">
+              <el-button size="mini" type="primary" @click="() => showParamsDialog.showDialog = true">
                 <el-icon class="el-icon-search" />
               </el-button>
               <el-button size="mini" type="danger" @click="deleteLog(scope.$index, scope.row)">
