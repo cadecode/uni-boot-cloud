ee36e0c1e75da1c34b8f3686cf7ffe39a05149f6
Cade Li <cade.li@qq.com>
2023-08-23 23:16:34 +0800

    feat: 添加消息队列管理页面

--- src/api/develop.js
+++ src/api/develop.js
@@ -18,3 +18,31 @@ export function deleteLog(serviceUrl, data) {
     messagePrefix: '删除日志'
   });
 }
+
+export function pageMqMsg(serviceUrl, data) {
+  return request({
+    url: serviceUrl + '/plugin/mq_msg/page',
+    method: 'post',
+    data
+  });
+}
+
+export function listMqMsgByIdList(serviceUrl, data) {
+  return request({
+    url: serviceUrl + '/plugin/mq_msg/list_by_id_list',
+    method: 'post',
+    data
+  });
+}
+
+export function updateMqMsg(serviceUrl, data) {
+  return request({
+    url: serviceUrl + '/plugin/mq_msg/update',
+    method: 'post',
+    data
+  }, {
+    messageFn: res => res.data,
+    messagePrefix: '更新MQ消息'
+  });
+}
+
