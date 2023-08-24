bae518db55c3811a76c6fd8b761a13159834a9c9
Cade Li <cade.li@qq.com>
2023-08-24 12:24:37 +0800

    docs: 维护 SQL 脚本，维护 menu 表、dict 表变更

--- .docs/sql/framework/data.sql
+++ .docs/sql/framework/data.sql
@@ -22,51 +22,56 @@ INSERT INTO sys_role (id, code, name, description)
 -- ----------------------------
 -- 插入菜单
 -- ----------------------------
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (1, null, 'Home', '/home/demo_home', '/Home/DemoHome', '首页', 1, 1, 1);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (1, null, 'Home', '/home/demo_home', '/Home/DemoHome', '首页', 1, 'el-icon-s-home', 1, 1, '2023-08-18 15:03:26',
+        '2023-08-24 10:28:24', 'admin');
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (2, null, 'System', '/system', '/system', '系统管理', 0, 'el-icon-setting', 2, 1, '2023-08-18 15:03:26',
+        '2023-08-24 10:28:43', 'admin');
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (3, null, 'Develop', '/develop', '/develop', '开发管理', 0, 'el-icon-s-platform', 2, 1, '2023-08-18 15:03:29',
+        '2023-08-24 10:28:58', 'admin');
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (201, 2, 'User', '/system/user', '/System/User', '用户管理', 1, null, 201, 1, '2023-08-18 15:03:27', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (202, 2, 'Role', '/system/role', '/System/Role', '角色管理', 1, null, 202, 1, '2023-08-18 15:03:27', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (203, 2, 'Menu', '/system/menu', '/System/Menu', '菜单管理', 1, null, 203, 1, '2023-08-18 15:03:28', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (204, 2, 'Api', '/system/api', '/System/Api', 'API管理', 1, null, 204, 1, '2023-08-18 15:03:28', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (205, 2, 'Dict', '/system/dict', '/System/Dict', '字典管理', 1, null, 205, 1, '2023-08-18 15:03:29', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (301, 3, 'Log', '/develop/log', '/Develop/Log', '日志管理', 1, null, 301, 1, '2023-08-18 15:03:30', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (302, 3, 'MqMsg', '/develop/mq_msg', '/Develop/MqMsg', '消息队列', 1, null, 302, 1, '2023-08-22 23:23:46',
+        '2023-08-24 10:30:42', 'admin');
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (306, 3, 'Icon', '/develop/icon', '/Develop/Icon', '图标', 1, null, 306, 1, '2023-08-18 15:03:30', null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (307, 3, 'Echarts', '/develop/echarts', '/Develop/Echarts', 'Echarts 示例', 1, null, 307, 1, '2023-08-18 15:03:30',
+        null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (308, 3, 'Swagger', 'http://localhost:8000/doc.html', null, 'Swagger', 1, null, 308, 1, '2023-08-18 15:03:30',
+        null, null);
+INSERT INTO uni_boot_dev.sys_menu (id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, icon,
+                                   order_num, enable_flag, create_time, update_time, update_user)
+VALUES (1000, null, 'GithubUrl', 'https://github.com/cadecode/uni-boot-admin', null, '源码仓库', 1, 'el-icon-star-on', 1000,
+        1, '2023-08-18 15:03:31', '2023-08-24 10:28:09', 'admin');
 
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (2, null, 'System', '/system', null, '系统管理', 0, 2, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (201, 2, 'User', '/system/user', '/System/User', '用户管理', 1, 201, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (202, 2, 'Role', '/system/role', '/System/Role', '角色管理', 1, 202, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (203, 2, 'Menu', '/system/menu', '/System/Menu', '菜单管理', 1, 203, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (204, 2, 'Api', '/system/api', '/System/Api', 'API管理', 1, 204, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (205, 2, 'Dict', '/system/dict', '/System/Dict', '字典管理', 1, 205, 1);
-
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (3, null, 'Develop', '/develop', null, '开发管理', 0, 2, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (301, 3, 'Log', '/develop/log', '/Develop/Log', '日志管理', 1, 301, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (306, 3, 'Icon', '/develop/icon', '/Develop/Icon', '图标', 1, 306, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (307, 3, 'Echarts', '/develop/echarts', '/Develop/Echarts', 'Echarts 示例', 1, 307, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (308, 3, 'Swagger', 'http://localhost:8000/doc.html', null, 'Swagger', 1, 308, 1);
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (309, 3, 'Druid', 'http://localhost:8000/druid', null, 'Druid', 1, 309, 1);
-
-INSERT INTO sys_menu(id, parent_id, route_name, route_path, component_path, menu_name, leaf_flag, order_num,
-                     enable_flag)
-    VALUE (1000, null, 'GithubUrl', 'https://github.com/cadecode/uni-boot-admin', null, '源码仓库', 1, 1000, 1);
 -- ----------------------------
 -- 插入 API
 -- ----------------------------
@@ -102,6 +107,8 @@ INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (1, 3);
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (1, 301);
+INSERT INTO sys_role_menu (role_id, menu_id)
+VALUES (1, 302);
 INSERT INTO sys_role_menu (role_id, menu_id)
 VALUES (1, 306);
 INSERT INTO sys_role_menu (role_id, menu_id)
@@ -146,43 +153,55 @@ VALUES (3, 1);
 -- ----------------------------
 -- 插入字典
 -- ----------------------------
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (1, '日志类型', 'logType', '查询', 'Query', '日志类型-查询', 0, 0, '2023-06-11 23:24:26',
-        '2023-06-12 00:11:57', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (2, '日志类型', 'logType', '删除', 'Remove', '日志类型-删除', 1, 0, '2023-06-11 23:24:52',
-        '2023-06-12 00:12:01', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (3, '日志类型', 'logType', '更新', 'Update', '日志类型-更新', 2, 0, '2023-06-11 23:25:08',
-        '2023-06-12 00:12:05', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (4, '日志类型', 'logType', '添加', 'Add', '日志类型-添加', 3, 0, '2023-06-11 23:25:29',
-        '2023-06-12 00:12:09', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (5, '日志类型', 'logType', '鉴权', 'Auth', '日志类型-鉴权', 4, 0, '2023-06-11 23:25:45',
-        '2023-06-12 00:12:15', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (6, '日志类型', 'logType', '导入', 'Import', '日志类型-导入', 5, 0, '2023-06-11 23:25:58',
-        '2023-06-12 00:12:19', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (7, '日志类型', 'logType', '导出', 'Export', '日志类型-导出', 6, 0, '2023-06-11 23:26:09',
-        '2023-06-12 00:12:30', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (8, '日志类型', 'logType', '上传', 'Upload', '日志类型-上传', 7, 0, '2023-06-11 23:26:26',
-        '2023-06-12 00:12:35', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (9, '日志类型', 'logType', '下载', 'Download', '日志类型-下载', 8, 0, '2023-06-11 23:26:40',
-        '2023-06-12 00:12:40', 'admin');
-INSERT INTO sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time, update_time,
-                      update_user)
-VALUES (10, '日志类型', 'logType', '其他', 'Other', '日志类型-其他', 9, 0, '2023-06-11 23:26:49',
-        '2023-06-12 00:52:14', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (1, '日志类型', 'logType', '查询', 'Query', '日志类型-查询', 0, 0, '2023-06-11 23:24:26', '2023-06-12 00:11:57', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (2, '日志类型', 'logType', '删除', 'Remove', '日志类型-删除', 1, 0, '2023-06-11 23:24:52', '2023-06-12 00:12:01', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (3, '日志类型', 'logType', '更新', 'Update', '日志类型-更新', 2, 0, '2023-06-11 23:25:08', '2023-06-12 00:12:05', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (4, '日志类型', 'logType', '添加', 'Add', '日志类型-添加', 3, 0, '2023-06-11 23:25:29', '2023-06-12 00:12:09', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (5, '日志类型', 'logType', '鉴权', 'Auth', '日志类型-鉴权', 4, 0, '2023-06-11 23:25:45', '2023-06-12 00:12:15', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (6, '日志类型', 'logType', '导入', 'Import', '日志类型-导入', 5, 0, '2023-06-11 23:25:58', '2023-06-12 00:12:19', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (7, '日志类型', 'logType', '导出', 'Export', '日志类型-导出', 6, 0, '2023-06-11 23:26:09', '2023-06-12 00:12:30', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (8, '日志类型', 'logType', '上传', 'Upload', '日志类型-上传', 7, 0, '2023-06-11 23:26:26', '2023-06-12 00:12:35', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (9, '日志类型', 'logType', '下载', 'Download', '日志类型-下载', 8, 0, '2023-06-11 23:26:40', '2023-06-12 00:12:40', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (10, '日志类型', 'logType', '其他', 'Other', '日志类型-其他', 9, 0, '2023-06-11 23:26:49', '2023-06-12 00:52:14', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (11, '服务URL', 'serviceUrl', '框架服务', '/framework', 'framework 服务 url 前缀', 1, 1, '2023-08-18 15:09:16',
+        '2023-08-24 10:20:50', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (12, '服务URL', 'serviceUrl', '示例服务', '/example', 'example 服务 url 前缀', 2, 0, '2023-08-18 15:09:30',
+        '2023-08-24 10:20:51', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (13, 'MQ消息发送状态', 'mqMsgSendState', '处理中', 'PREPARING', 'MQ 消息发送中', 1, 1, '2023-08-22 23:25:52',
+        '2023-08-24 10:20:50', 'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (14, 'MQ消息发送状态', 'mqMsgSendState', '失败', 'FAIL', 'MQ 消息发送失败', 2, 1, '2023-08-22 23:26:04', '2023-08-24 10:20:51',
+        'admin');
+INSERT INTO uni_boot_dev.sys_dict (id, name, type, label, value, description, order_num, default_flag, create_time,
+                                   update_time, update_user)
+VALUES (15, 'MQ消息发送状态', 'mqMsgSendState', '完成', 'OVER', 'MQ 消息发送完成', 3, 0, '2023-08-22 23:26:10', '2023-08-24 10:20:50',
+        'admin');
+
+
