e9482853953610bb6a23dfadc2db8799a3b40f92
Cade Li <cade.li@qq.com>
2023-10-13 13:44:13 +0800

    fix: gateway 去除 tomcat 依赖

--- gateway/src/main/java/com/github/cadecode/uniboot/gateway/UniGatewayApplication.java
+++ gateway/src/main/java/com/github/cadecode/uniboot/gateway/UniGatewayApplication.java
@@ -3,8 +3,6 @@ package com.github.cadecode.uniboot.gateway;
 import com.github.cadecode.uniboot.common.core.extension.strategy.StrategyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -18,14 +16,9 @@ import org.springframework.plugin.core.config.EnablePluginRegistries;
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
-public class UniGatewayApplication extends SpringBootServletInitializer {
+public class UniGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniGatewayApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(UniGatewayApplication.class);
     }
 }
