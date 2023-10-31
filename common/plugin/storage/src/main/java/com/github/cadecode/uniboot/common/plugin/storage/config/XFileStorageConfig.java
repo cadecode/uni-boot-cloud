80709bc3abe53e24076f283ffb48e98265d72e33
Cade Li <cadecode@foxmail.com>
2023-10-31 08:42:42 +0800

    refactor: 调整 fileRecorder bean 声明位置

--- common/plugin/storage/src/main/java/com/github/cadecode/uniboot/common/plugin/storage/config/XFileStorageConfig.java
+++ common/plugin/storage/src/main/java/com/github/cadecode/uniboot/common/plugin/storage/config/XFileStorageConfig.java
@@ -1,6 +1,10 @@
 package com.github.cadecode.uniboot.common.plugin.storage.config;
 
+import com.github.cadecode.uniboot.common.plugin.storage.handler.AbstractStorageHandler;
+import lombok.RequiredArgsConstructor;
+import org.dromara.x.file.storage.core.recorder.FileRecorder;
 import org.dromara.x.file.storage.spring.EnableFileStorage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,7 +14,12 @@ import org.springframework.context.annotation.Configuration;
  * @since 2023/10/25
  */
 @EnableFileStorage
+@RequiredArgsConstructor
 @Configuration
 public class XFileStorageConfig {
 
+    @Bean
+    public FileRecorder fileRecorder(AbstractStorageHandler storageRecordHandler) {
+        return storageRecordHandler;
+    }
 }
