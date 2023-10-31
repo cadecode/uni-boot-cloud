fc5e27e236e4e27475a3ef9abc41018839c9c375
Cade Li <cade.li@qq.com>
2023-10-31 15:04:31 +0800

    feat: 添加根据 username 查用户信息 feign 接口

---
+++ framework/framework_api/src/main/java/com/github/cadecode/uniboot/framework/api/feignclient/SysUserClient.java
@@ -0,0 +1,20 @@
+package com.github.cadecode.uniboot.framework.api.feignclient;
+
+import com.github.cadecode.uniboot.framework.api.bean.dto.SysUserDto.SysUserGetByUsernameResDto;
+import com.github.cadecode.uniboot.framework.api.consts.SvcNameConst;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * SysApi feign client
+ *
+ * @author Cade Li
+ * @since 2023/7/28
+ */
+@FeignClient(contextId = "SysUserClient", name = SvcNameConst.FRAMEWORK)
+public interface SysUserClient {
+
+    @PostMapping("system/user/client/get_by_username")
+    SysUserGetByUsernameResDto getByUsername();
+
+}
