049c203a8e579da87d639023751a556695341f45
Cade Li <cadecode@foxmail.com>
2024-03-09 12:36:32 +0800

    revert: 移除 shardingjdbc 相关依赖及设施

--- example/example_svc/src/main/java/com/github/cadecode/uniboot/example/svc/controller/DynamicDsController.java
+++ example/example_svc/src/main/java/com/github/cadecode/uniboot/example/svc/controller/DynamicDsController.java
@@ -1,11 +1,7 @@
 package com.github.cadecode.uniboot.example.svc.controller;
 
 import com.baomidou.dynamic.datasource.annotation.DS;
-import com.github.cadecode.uniboot.common.plugin.datasource.consts.DsConst;
 import com.github.cadecode.uniboot.framework.base.annotation.ApiFormat;
-import com.github.cadecode.uniboot.framework.base.plugin.bean.po.PlgLog;
-import com.github.cadecode.uniboot.framework.base.plugin.service.PlgLogService;
-import com.github.pagehelper.PageHelper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
-import java.util.List;
 
 /**
  * dynamic-datasource 测试
@@ -28,7 +23,7 @@ import java.util.List;
 @ApiFormat
 @Slf4j
 @RequiredArgsConstructor
-@Api(tags = "动态数据源测试")
+@Api(tags = "dynamic-datasource 测试")
 @DependsOn("dataSource")
 @RestController
 @RequestMapping("demo/ds")
@@ -38,8 +33,6 @@ public class DynamicDsController {
      * 注入 Spring 数据源，用来查看 jdbc url 验证数据源切换
      */
     private final AbstractDataSource dataSource;
-
-    private final PlgLogService plgLogService;
 
     @DS("master")
     @ApiOperation("测试 master 数据源")
@@ -53,25 +46,6 @@ public class DynamicDsController {
     @PostMapping("test_slave1")
     public String testSlave1() throws SQLException {
         return dataSource.getConnection().getMetaData().getURL();
-    }
-
-    @DS(DsConst.DS_NAME_SHARDING)
-    @ApiOperation("测试 sharding 数据源读取")
-    @PostMapping("test_sharding_read")
-    public List<PlgLog> testShardingRead() throws SQLException {
-        return PageHelper.startPage(0, 10)
-                .<PlgLog>doSelectPageInfo(plgLogService::list).getList();
-    }
-
-    @DS(DsConst.DS_NAME_SHARDING)
-    @ApiOperation("测试 sharding 数据源写入")
-    @PostMapping("test_sharding_write")
-    public boolean testShardingWrite() throws SQLException {
-        return plgLogService.save(PlgLog.builder()
-                .logType("测试LOG")
-                .exceptional(false)
-                .classMethod("DynamicDsController.testShardingWriteAndRead")
-                .build());
     }
 
 }
