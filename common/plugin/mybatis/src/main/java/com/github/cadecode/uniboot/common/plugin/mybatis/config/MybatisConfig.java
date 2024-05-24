9fbd235087b6d117dba336b237ce7aa7bd752428
Cade Li <cadecode@foxmail.com>
2024-05-24 08:28:36 +0800

    fix: 在没有使用注解时，跳过 DataPermissionInterceptor

--- common/plugin/mybatis/src/main/java/com/github/cadecode/uniboot/common/plugin/mybatis/config/MybatisConfig.java
+++ common/plugin/mybatis/src/main/java/com/github/cadecode/uniboot/common/plugin/mybatis/config/MybatisConfig.java
@@ -2,9 +2,19 @@ package com.github.cadecode.uniboot.common.plugin.mybatis.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.DataPermissionInterceptor;
+import com.github.cadecode.uniboot.common.plugin.mybatis.aspect.DataScopeAspect;
+import com.github.cadecode.uniboot.common.plugin.mybatis.aspect.DataScopeAspect.DataScopeParam;
 import com.github.cadecode.uniboot.common.plugin.mybatis.handler.DataScopePermissionHandler;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+import java.util.Objects;
 
 /**
  * mybatis 配置类
@@ -22,7 +32,17 @@ public class MybatisConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         // 数据权限插件
-        DataPermissionInterceptor dataPermissionInterceptor = new DataPermissionInterceptor(new DataScopePermissionHandler());
+        DataPermissionInterceptor dataPermissionInterceptor = new DataPermissionInterceptor(new DataScopePermissionHandler()) {
+            @Override
+            public void beforeQuery(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
+                // 没有使用注解时，跳过
+                DataScopeParam dataScopeParam = DataScopeAspect.currDataScope();
+                if (Objects.isNull(dataScopeParam) || !dataScopeParam.isEnableFilter()) {
+                    return;
+                }
+                super.beforeQuery(executor, ms, parameter, rowBounds, resultHandler, boundSql);
+            }
+        };
         interceptor.addInnerInterceptor(dataPermissionInterceptor);
 
         // 添加分页插件，动态获取数据库类型
