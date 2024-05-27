4b16fba8a37726a4fb877d4f4c2b4663dfb30ef7
Cade Li <cade.li@qq.com>
2024-05-27 08:04:33 +0800

    feat: 添加权限相关工具方法，获取角色 tpyeCode

--- src/util/permission.js
+++ src/util/permission.js
@@ -18,6 +18,32 @@ function checkPermission(value) {
   }
 }
 
+/**
+ * 拼接角色和 type 和 code
+ */
+function getRoleTypeCode(role) {
+  return `${role.type}:${role.code}`;
+}
+
+const ROLE_TYPE_ACCESS = 'ACCESS';
+const ROLE_TYPE_DATA = 'DATA';
+const ROLE_TYPES = [
+  {
+    label: '访问角色',
+    value: ROLE_TYPE_ACCESS
+  },
+  {
+    label: '数据角色',
+    value: ROLE_TYPE_DATA
+  }];
+const roleTypes = {
+  ROLE_TYPE_ACCESS,
+  ROLE_TYPE_DATA,
+  ROLE_TYPES
+};
+
 export {
-  checkPermission
+  checkPermission,
+  getRoleTypeCode,
+  roleTypes
 };
