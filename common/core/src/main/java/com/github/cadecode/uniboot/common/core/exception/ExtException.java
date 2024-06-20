a4f34103708727e8b314c6608b9c7e39f22819b6
Cade Li <cade.li@qq.com>
2024-06-20 09:59:02 +0800

    feat: 优化顶级异常，支持异常信息字符串模板

--- common/core/src/main/java/com/github/cadecode/uniboot/common/core/exception/ExtException.java
+++ common/core/src/main/java/com/github/cadecode/uniboot/common/core/exception/ExtException.java
@@ -6,24 +6,23 @@ package com.github.cadecode.uniboot.common.core.exception;
  * @author Cade Li
  * @since 2023/6/24
  */
-public class ExtException extends RuntimeException {
+public class ExtException extends BaseException {
     public ExtException() {
-        super();
     }
 
-    public ExtException(String message) {
-        super(message);
+    public ExtException(String message, Object... params) {
+        super(message, params);
     }
 
-    public ExtException(String message, Throwable cause) {
-        super(message, cause);
+    public ExtException(String message, Throwable cause, Object... params) {
+        super(message, cause, params);
     }
 
     public ExtException(Throwable cause) {
         super(cause);
     }
 
-    protected ExtException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public ExtException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, Object... params) {
+        super(message, cause, enableSuppression, writableStackTrace, params);
     }
 }
--- common/core/src/main/java/com/github/cadecode/uniboot/common/core/exception/UtilException.java
+++ common/core/src/main/java/com/github/cadecode/uniboot/common/core/exception/UtilException.java
@@ -6,23 +6,23 @@ package com.github.cadecode.uniboot.common.core.exception;
  * @author Cade Li
  * @date 2023/6/9
  */
-public class UtilException extends RuntimeException {
+public class UtilException extends BaseException {
     public UtilException() {
     }
 
-    public UtilException(String message) {
-        super(message);
+    public UtilException(String message, Object... params) {
+        super(message, params);
     }
 
-    public UtilException(String message, Throwable cause) {
-        super(message, cause);
+    public UtilException(String message, Throwable cause, Object... params) {
+        super(message, cause, params);
     }
 
     public UtilException(Throwable cause) {
         super(cause);
     }
 
-    public UtilException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public UtilException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, Object... params) {
+        super(message, cause, enableSuppression, writableStackTrace, params);
     }
 }
